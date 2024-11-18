a216719a360cccdb2beedcd127a4021e41fd0171
Valentin Dassonville <valentin.dassonville59@gmail.com>
2024-11-18 11:52:54 +0100

    Environment installation script

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# website
+# Amnesty International France Website
+
+Ceci est le dépôt de la refonte du site web d'Amnesty International France basé sur [humanity theme](https://github.com/amnestywebsite/humanity-theme). Le dépôt contient tous les éléments nécessaires au fonctionnement du site, notamment les plugins spécifiques.
+
+This is the repository for the redesign of the Amnesty International France website based on [humanity theme](https://github.com/amnestywebsite/humanity-theme). The repository contains all the elements required to run the site, including specific plugins.
+
+## Requirements
+
+- PHP 8.0 or greater
+- MySQL version 8.0 or greater or MariaDB version 10.5 or greater
+- [Castor](https://github.com/jolicode/castor)
+
+## Installation
+
+The first step is to change `.env` file to provide some information to the script for the creation of the WordPress environment.  
+You can create different env file like `.env.local`, `.env.dev`, ...
+
+The script takes one argument which is the path. It corresponds to the location where the environment will be created (default: current folder).
+
+To start the script : `castor install <path>`
+
+It will install the environment with the [humanity theme](https://github.com/amnestywebsite/humanity-theme) and its required plugins.
+
+## Custom Plugins
+
+A custom plugin must be developed directly in a folder under `<path>/wp-content/plugins` (example: wp-content/plugins/hello-world).  
+This will allow the plugin to be loaded directly into wordpress. Note that it is not activated by default, so you need to activate it in wp-admin.
+
+For your plugin to be versioned on git, you need to add the following line to the plugins section of the `.gitignore` file : `!wp-content/plugins/your-plugin/`
+
+## Start wordpress
+
+To start wordpress, run : `wp server`
---
+++ castor.php
@@ -0,0 +1,76 @@
+<?php
+
+use Castor\Attribute\AsTask;
+
+use function Castor\context;
+use function Castor\fs;
+use function Castor\http_client;
+use function Castor\http_download;
+use function Castor\io;
+use function Castor\load_dot_env;
+use function Castor\run;
+
+#[AsTask(description: 'Install wordpress with humanity theme and plugins')]
+function install(string $path = '.'): void
+{
+    io()->title("Installation of WordPress for Amnesty International France");
+
+    if(!fs()->exists('.env')) {
+        io()->error('.env file not found');
+        exit(1);
+    }
+
+    if(!fs()->exists($path)) {
+       fs()->mkdir($path);
+    }
+
+    $context = context()->withEnvironment(load_dot_env())->withWorkingDirectory($path)->withAllowFailure();
+
+    $wp_cli_installed = run('wp cli', context: $context->withQuiet());
+    if(!$wp_cli_installed->isSuccessful()) {
+        io()->info('wp cli is not installed'.PHP_EOL.'Installation...');
+
+        http_download('https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar', './wp-cli.phar');
+        $result = run(['php', 'wp-cli.phar', '--info']);
+        if(!$result->isSuccessful()) {
+            io()->error('Error during the installation of wp cli');
+            fs()->remove('wp-cli.phar');
+            exit(1);
+        }
+        fs()->chmod('wp-cli.phar', 0755);
+        fs()->copy('./wp-cli.phar', '/usr/local/bin/wp');
+        fs()->remove('./wp-cli.phar');
+        io()->success('wp cli installation complete');
+    }
+
+    // Test if a WordPress installation exists
+    $wp_installation = run('wp core is-installed', context: $context->withQuiet());
+    if($wp_installation->isSuccessful()) {
+        io()->info('A wordpress installation already exists');
+        exit(1);
+    }
+
+    io()->info("Starting installation of WordPress environment");
+    run('wp core download --locale=$LANG --skip-content --path=.', context: $context);
+    run('wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --dbprefix=$DB_PREFIX', context: $context);
+    run('wp db create', context: $context);
+    run('wp core install --locale=$LANG --url=$WP_URL --title="$WP_TITLE" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL', context: $context);
+    $theme_version = get_github_latest_version('https://github.com/amnestywebsite/humanity-theme/releases/latest');
+    run("wp theme install https://github.com/amnestywebsite/humanity-theme/releases/download/$theme_version/humanity-theme.zip --activate", context: $context);
+    $cmb2_attached_posts_version = get_github_latest_version('https://github.com/CMB2/cmb2-attached-posts/releases/latest');
+    $cmb2_password_field_version = get_github_latest_version('https://github.com/amnestywebsite/cmb2-password-field/releases/latest');
+    $cmb2_message_field_version = get_github_latest_version('https://github.com/amnestywebsite/cmb2-message-field/releases/latest');
+    $cmb2_field_order_version = get_github_latest_version('https://github.com/jaymcp/cmb2-field-order/releases/latest');
+    run("wp plugin install cmb2 \
+        https://github.com/CMB2/cmb2-attached-posts/archive/refs/tags/$cmb2_attached_posts_version.zip \
+        https://github.com/amnestywebsite/cmb2-password-field/releases/download/$cmb2_password_field_version/cmb2-password-field.zip \
+        https://github.com/amnestywebsite/cmb2-message-field/releases/download/$cmb2_message_field_version/cmb2-message-field.zip \
+        https://github.com/jaymcp/cmb2-field-order/archive/refs/tags/$cmb2_field_order_version.zip \
+        --activate", context: $context);
+}
+
+function get_github_latest_version(string $url): string {
+    $location = http_client()->withOptions(['max_redirects' => 0])->request('GET', $url)->getHeaders(false)['location'][0];
+    $split = explode('/', $location);
+    return $split[array_key_last($split)];
+}
