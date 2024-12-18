0b1db17362f071bfcb57d395002128cfba5bd2b2
Volubyl <volubyl@pm.me>
2024-12-18 16:42:36 +0100

    feat: register user account (#2)
    * create account creation form
    
    * feat-register-user-form
    
    * fix : a11y

--- wp-content/themes/espace-donateur/templates/check-email.php
+++ wp-content/themes/espace-donateur/templates/check-email.php
@@ -6,49 +6,43 @@ $success_message = "";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email'])) {
 
-	$data = get_salesforce_user_data($_POST['email']);
-	if (isset($data->hasEspaceDon)) {
 
-		if ($data->hasEspaceDon) {
-			$success_message = "Redirection vers la page de connexion";
+    if (has_access_to_donation_space($_POST['email'])) {
 
-			// wp_redirect(get_permalink( get_page_by_path( 'creer-un-compte' ) ));
-		} else {
-			$success_message = "L'utilisateur n'a pas accès à l'espace don";
-		}
-	}
+        wp_redirect(get_permalink(get_page_by_path('creer-un-compte')));
+        exit;
+    } else {
+        $error_message = "Vous n'avez pas accès à l'espace don";
+    }
+
 }
 ?>
 
-<?php if (isset($success_message)) : ?>
-	<div class="success-message"><?php echo $success_message; ?></div>
-<?php endif; ?>
 
 
 
 <main class="wp-block-group is-layout-flow wp-block-group-is-layout-flow">
-
-	<div class="container">
-
-
-		<header class="wp-block-group article-header is-layout-flow wp-block-group-is-layout-flow">
-			<h1 class="article-title wp-block-post-title">Qui êtes vous ?</h1>
-		</header>
+    <?php if (isset($error_message)) : ?>
+    <div class="error-message"><?php echo $success_message; ?></div>
+    <?php endif; ?>
+    <div class="container">
 
 
+        <header class="wp-block-group article-header is-layout-flow wp-block-group-is-layout-flow">
+            <h1 class="article-title wp-block-post-title">Qui êtes vous ?</h1>
+        </header>
 
-		<p>Nous avons besoin de votre email pour déterminer si vous avez êtes déja connu</p>
+        <p>Nous avons besoin de votre email pour déterminer si vous avez êtes déja connu</p>
 
-		<form role="form" method="POST" action="">
-			<label>Votre adresse email</label>
-			<div>
-				<input placeholder="" value="" type="email" name="email" required="true">
-				<button aria-label="Rechercher" class="btn btn--dark" type="submit">Vérifier votre email</button>
-			</div>
-		</form>
+        <form role="form" method="POST" action="">
+            <label>Votre adresse email</label>
+            <div>
+                <input placeholder="" value="" type="email" name="email" required="true">
+                <button aria-label="Rechercher" class="btn btn--dark" type="submit">Vérifier votre email</button>
+            </div>
+        </form>
 
 
-
-	</div>
+    </div>
 
 </main>
