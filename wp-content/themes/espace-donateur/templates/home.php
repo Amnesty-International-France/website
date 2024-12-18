68dbc6ea6c0e0b5b7a62f2e8c4628918ef3e3023
Volubyl <volubyl@pm.me>
2024-12-18 16:59:14 +0100

    feat: login user (#4)
    * add amnesty branding
    
    * create account creation form
    
    * feat-register-user-form
    
    * fix : a11y
    
    * feat: login user
    
    * fix login

--- wp-content/themes/espace-donateur/templates/home.php
+++ wp-content/themes/espace-donateur/templates/home.php
@@ -14,7 +14,7 @@ get_header();
 
 
         <header class="wp-block-group article-header is-layout-flow wp-block-group-is-layout-flow">
-            <h1 class="article-title wp-block-post-title">Home page</h1>
+            <h1 class="article-title wp-block-post-title">Mon espace donateur</h1>
         </header>
 
 
