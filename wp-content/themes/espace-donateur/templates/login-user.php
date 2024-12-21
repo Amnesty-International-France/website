7d214b4caf324648f70ca445eb4f17c08cf2f6eb
Volubyl <volubyl@pm.me>
2024-12-21 13:53:21 +0100

    feat: create user session

--- wp-content/themes/espace-donateur/templates/login-user.php
+++ wp-content/themes/espace-donateur/templates/login-user.php
@@ -7,16 +7,32 @@ get_header();
 
 $success_message = "";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email']) && isset($_POST['password'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['login_nonce'])) {
 
-    $user = get_user_by('email', $_POST['email']);
-    if($user) {
-        $password_match = wp_check_password($_POST['password'], $user->user_pass, $user->ID);
-        if($password_match) {
-            wp_redirect(get_permalink(get_page_by_path('accueil')));
-            exit;
-        }
+    $email = sanitize_text_field($_POST['email']);
+    $password = $_POST['password'] ;
+
+
+    if (!isset($_POST['login_nonce']) || !wp_verify_nonce($_POST['login_nonce'], 'login_form')) {
+        die('Invalid nonce.');
     }
+
+    $creds = array(
+        'user_login'    => $email,
+        'user_password' => $password,
+        'remember'      => true
+    );
+
+    $user = wp_signon($creds, false);
+
+    if (!is_wp_error($user)) {
+
+        wp_set_current_user($user->ID);
+        wp_redirect(get_permalink(get_page_by_path('accueil')));
+        exit;
+    }
+
+
 
     $error_message = "Utilisateur inconnu ou mauvais mot de passe";
 
@@ -43,10 +59,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email']) && isset($_P
         <p>Connectez-vous pour accéder à votre espace donateur</p>
 
         <form role="form" method="POST" action="">
-            <label>Votre adresse email (obligatoire)</label>
-            <input placeholder="" value="" type="email" name="email" required="true">
-            <label>Votre mot de passe (obligatoire)</label>
-            <input placeholder="" value="" type="password" name="password" required="true">
+
+            <?php wp_nonce_field('login_form', 'login_nonce'); ?>
+            <label for="email">Votre adresse email (obligatoire)</label>
+            <input placeholder="" value="" type="email" name="email" id="email" autocomplete="email" required="true">
+            <label for="email">Votre mot de passe (obligatoire)</label>
+            <input placeholder="" id="password" value="" type="password" name="password" autocomplete="password"
+                required="true">
 
             <button class="btn btn--dark" type="submit">Se connecter</button>
 
