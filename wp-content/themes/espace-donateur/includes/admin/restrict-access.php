d23444f3c3baf40a1b4ed22a2c5bb13f54add3e4
Volubyl <volubyl@pm.me>
2024-12-21 13:53:21 +0100

    feat: create user session

---
+++ wp-content/themes/espace-donateur/includes/admin/restrict-access.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Functions file
+ *
+ */
+
+// phpcs:disable Squiz.Commenting.InlineComment.WrongStyle,PEAR.Commenting.InlineComment.WrongStyle
+
+/**
+ * Theme root includes
+ */
+#region Admin Access
+require_once realpath(__DIR__ . '/includes/admin/restrict-access.php');
+#endregion helpers
+
+
+
+
+
+// phpcs:enable Squiz.Commenting.InlineComment.WrongStyle,PEAR.Commenting.InlineComment.WrongStyle
