a96811091950bf29602d04349665e569c9d40dd9
BertrandD. <volubyl@pm.me>
2024-12-24 12:14:00 +0100

    feat : improve ui

--- wp-content/themes/espace-donateur/includes/2FA/index.php
+++ wp-content/themes/espace-donateur/includes/2FA/index.php
@@ -38,7 +38,7 @@ function send_2fa_code($to_email, $code, $verification_url)
             'from' => 'noreply@' . MAILGUN_DOMAIN,
             'to' => $to_email,
             'subject' => 'Amnesty France- Vérifier votre email',
-            'text' => 'Le code de vérification est ' . $code . '. Cliquer ici pour activer votre compte: ' . $verification_url
+            'text' => 'Le code de vérification est ' . $code . 'Cliquer ici pour activer votre compte: ' . $verification_url
         ),
         'headers' => array(
             'Authorization' => 'Basic ' . base64_encode('api:' . $api_key)
@@ -50,4 +50,45 @@ function send_2fa_code($to_email, $code, $verification_url)
     } else {
         return true;
     }
+}
+
+function get_login_blocked_until($user_id)
+{
+
+    return get_user_meta($user_id, 'login_blocked_until', true);
+}
+
+function can_check_code($user_id)
+{
+
+    $blocked_until = get_login_blocked_until($user_id);
+    return $blocked_until && $blocked_until <= time();
+
+}
+
+function limit_login_attempts($user_id)
+{
+
+    if (can_check_code($user_id)) {
+        reset_login_attempts($user_id);
+    }
+
+    $attempts = (int)get_user_meta($user_id, 'login_attempts', true) ?? 0;
+
+
+    if ($attempts >= 10) {
+        update_user_meta($user_id, 'login_blocked_until', time() + 3600); // 1 heure
+        return false;
+    } else {
+
+        print_r($attempts);
+        update_user_meta($user_id, 'login_attempts', $attempts + 1);
+        return true;
+    }
+}
+
+function reset_login_attempts($user_id)
+{
+    update_user_meta($user_id, 'login_attempts', 0);
+    delete_user_meta($user_id, 'login_blocked_until');
 }
