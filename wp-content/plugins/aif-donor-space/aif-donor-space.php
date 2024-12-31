7a755f70d402c4bae7b5c23f60faa3a37d1bae0b
BertrandD. <volubyl@pm.me>
2024-12-31 15:37:09 +0100

    chore : reorganize folders and files

--- wp-content/plugins/aif-donor-space/aif-donor-space.php
+++ wp-content/plugins/aif-donor-space/aif-donor-space.php
@@ -49,7 +49,8 @@ require_once AIF_DONOR_SPACE_PATH. '/includes/domain/tax-receipt/index.php';
 /*
 /  Domain
 */
-require_once plugin_dir_path(__FILE__) . '/includes/domain/tax-receipt.php';
+require_once plugin_dir_path(__FILE__) . '/includes/domain/tax-receipt/rest-controllers.php';
+require_once plugin_dir_path(__FILE__) . '/includes/domain/tax-receipt/index.php';
 
 
 /*
@@ -139,56 +140,8 @@ function aif_donor_space_enqueue_assets()
 
 }
 add_action('wp_enqueue_scripts', 'aif_donor_space_enqueue_assets');
-add_action('rest_api_init', function () {
-    register_rest_route('aif-donor-space/v1', '/duplicate-tax-receipt-request/', array(
-        'methods' => 'POST',
-        'callback' => 'handle_duplicate_tax_receipt_request',
-        'permission_callback' => 'check_nonce',
-    ));
-});
-
-function handle_duplicate_tax_receipt_request(WP_REST_Request $request)
-{
-
-    $params = $request->get_json_params();
-
-    $userID = get_current_user_id();
-    $SF_ID = get_SF_user_ID($userID);
-    $taxt_receipt_reference = $params['taxReceiptReference'];
-
-    if (!$SF_ID || !$taxt_receipt_reference) {
-        return new WP_REST_Response(array('status' => 403,'message' => 'tax receipt ID not provided'));
-    }
 
 
-
-    $result = create_duplicate_taxt_receipt_request($SF_ID, $taxt_receipt_reference);
-
-    // if (!$result['success']) {
-    //     return new WP_REST_Response(['message' => 'demand failed'], status: 400);
-    // }
-
-    $response = array(
-        'message' => 'Tax receipt duplicated successfully!',
-        'result' => $result
-    );
-    return new WP_REST_Response($response, 200);
-}
-
-function check_nonce(WP_REST_Request $request)
-{
-
-    $nonce = $request->get_header('X-WP-Nonce');
-
-    if (!wp_verify_nonce($nonce, 'wp_rest')) {
-        return new WP_Error('rest_forbidden', 'Invalid nonce.', array('status' => 403));
-    }
-
-    if (!is_user_logged_in()) {
-        return new WP_Error('rest_forbidden', 'Not logged in.', array('status' => 403));
-
-    }
-
-
-    return true;
-}
+/**
+ *  REST endpoints
+ */
