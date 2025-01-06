c6e25fa48c5544c65e944d0581c9698ed8b19ea1
BertrandD. <volubyl@pm.me>
2025-01-06 10:59:06 +0100

    fix: refresh token

--- wp-content/plugins/aif-donor-space/includes/sales-force/authentification.php
+++ wp-content/plugins/aif-donor-space/includes/sales-force/authentification.php
@@ -2,10 +2,16 @@
 
 function get_salesforce_access_token()
 {
-    // Vérifie si un token d'accès valide est déjà stocké
     $access_token = get_option('salesforce_access_token');
-    $expiration_time = get_option('salesforce_token_expiration_time');
+    $expiration_time = intval(get_option('salesforce_token_expiration_time'));
 
+    $current_time_in_ms  = floor(microtime(true) * 1000);
+
+    $is_valid = $expiration_time > $current_time_in_ms;
+
+    if ($is_valid) {
+        return $access_token;
+    }
 
     return refresh_salesforce_token();
 }
@@ -13,22 +19,16 @@ function get_salesforce_access_token()
 
 function refresh_salesforce_token()
 {
-    // Paramètres de la requête pour rafraîchir le token
     $client_id = getenv('AIF_SALESFORCE_CLIENT_ID');
     $client_secret = getenv('AIF_SALESFORCE_SECRET');
-
-    // URL de l'API Salesforce (URL de production)
     $url = getenv("AIF_SALESFORCE_URL") . 'services/oauth2/token';
 
-
-    // Paramètres pour la requête POST
     $params = array(
         'grant_type'    => 'client_credentials',
         'client_id'     => $client_id,
         'client_secret' => $client_secret,
     );
 
-    // Effectuer la requête POST via wp_remote_post()
     $response = wp_remote_post($url, array(
         'method'    => 'POST',
         'body'      => $params,
@@ -39,27 +39,23 @@ function refresh_salesforce_token()
     ));
 
 
-
-    // Vérifier si la requête a réussi
     if (is_wp_error($response)) {
-
         return new WP_Error('request_failed', 'La requête a échoué', $response->get_error_message());
     }
 
-    // Analyser la réponse JSON
     $body = wp_remote_retrieve_body($response);
 
     $data = json_decode($body, true);
 
-
-
-    // Si l'authentification a réussi, on obtient un nouveau access_token
     if (isset($data['access_token'])) {
-        // Sauvegarder le nouveau token et sa date d'expiration
-        update_option('salesforce_access_token', $data['access_token']);
-        update_option('salesforce_token_expiration_time', time() + $data['issued_at']); // La durée d'expiration est donnée en secondes
 
-        // Retourner le nouveau access_token
+        $issued_at = intval($data['issued_at']); // warning : this is ms and not seconds
+        $expiration_interval = 10 * 60 * 1000; // 10 minutes in milliseconds
+        $expiration_time = $issued_at + $expiration_interval ;
+
+        update_option('salesforce_access_token', $data['access_token']);
+        update_option('salesforce_token_expiration_time', $expiration_time);
+
         return $data['access_token'];
     } else {
         return new WP_Error('token_refresh_failed', 'Le rafraîchissement du token a échoué', $data);
