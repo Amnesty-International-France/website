73038dc0429eec04993a47c4ab3ddb1de8e50112
BertrandD. <volubyl@pm.me>
2025-01-14 12:07:43 +0100

    chore: create partial function

--- wp-content/plugins/aif-donor-space/templates/create-account.php
+++ wp-content/plugins/aif-donor-space/templates/create-account.php
@@ -3,10 +3,7 @@
 /* Template Name: Create account */
 get_header();
 
-$error_all_fields_required_message = "";
-$error_invalid_email_message  = "";
-$error_password_not_match_message  = "";
-$error_technical_message  = "";
+$error_message = "";
 $error_no_access_to_donor_space = false;
 
 
@@ -16,12 +13,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $confirm_password = sanitize_text_field($_POST['confirm-password']);
 
     if (empty($email) || empty($password)) {
-        $error_all_fields_required_message = "Veuillez renseigner le mot de passe et votre email";
+        $error_message = "Veuillez renseigner le mot de passe et votre email";
     } elseif (!is_email($email)) {
-        $error_invalid_email_message = "L'email renseigné est invalide";
+        $error_message = "L'email renseigné est invalide";
 
     } elseif ($password !== $confirm_password) {
-        $error_password_not_match_message  = "Les mots de passe ne correspondent pas";
+        $error_message  = "Les mots de passe ne correspondent pas";
     } else {
 
         $sf_member = get_salesforce_member_data($email);
@@ -87,65 +84,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             votre don.
         </p>
 
-        <?php if (!empty($error_all_fields_required_message) || !empty($error_invalid_email_message) || !empty($error_password_not_match_message) || !empty($error_technical_message)) : ?>
-        <div class="aif-bg-grey--lighter aif-p1w aif-mb1w">
+        <?php if (!empty($error_message)) : ?>
 
-            <div class="aif-flex aif-gap-single">
-                <div>
-                    <svg aria-hidden="true" width="4" height="14" viewBox="0 0 4 14" fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M2 14C1.45 14 0.979167 13.8477 0.5875 13.5431C0.195833 13.2384 0 12.8722 0 12.4444C0 12.0167 0.195833 11.6505 0.5875 11.3458C0.979167 11.0412 1.45 10.8889 2 10.8889C2.55 10.8889 3.02083 11.0412 3.4125 11.3458C3.80417 11.6505 4 12.0167 4 12.4444C4 12.8722 3.80417 13.2384 3.4125 13.5431C3.02083 13.8477 2.55 14 2 14ZM0 9.33333V0H4V9.33333H0Z"
-                            fill="#D51118" />
-                    </svg>
-                </div>
-                <div>
-                    <p class="aif-text-red aif-text-bold">Une erreur est survenue</p>
+        <?php
+            $title = "Une erreur est survenue";
+            aif_include_partial("alert", [
+                "title" => $title,
+            "content" => $error_message])
 
-                    <p class="aif-mb0">
-                        <?= $error_all_fields_required_message  ?>
-                        <?= $error_invalid_email_message  ?>
-                        <?= $error_password_not_match_message  ?>
-                        <?= $error_technical_message  ?>
-                    </p>
-
-                </div>
-            </div>
-        </div>
-
+            ?>
         <?php endif; ?>
 
         <?php if ($error_no_access_to_donor_space) : ?>
-        <div class="aif-bg-grey--lighter aif-p1w aif-mb1w">
 
-            <div class="aif-flex aif-gap-single">
-                <div>
-                    <svg aria-hidden="true" width="4" height="14" viewBox="0 0 4 14" fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M2 14C1.45 14 0.979167 13.8477 0.5875 13.5431C0.195833 13.2384 0 12.8722 0 12.4444C0 12.0167 0.195833 11.6505 0.5875 11.3458C0.979167 11.0412 1.45 10.8889 2 10.8889C2.55 10.8889 3.02083 11.0412 3.4125 11.3458C3.80417 11.6505 4 12.0167 4 12.4444C4 12.8722 3.80417 13.2384 3.4125 13.5431C3.02083 13.8477 2.55 14 2 14ZM0 9.33333V0H4V9.33333H0Z"
-                            fill="#D51118" />
-                    </svg>
-                </div>
-                <div>
-                    <p class="aif-text-red aif-text-bold">L’adresse email renseignée ne trouve pas de
-                        correspondance
-                        dans notre
-                        système.</p>
+        <?php
 
-                    <p class="aif-mb0">
+                $title = "L’adresse email renseignée ne trouve pas de correspondance dans notre système.";
+            $content = 'Devenez donateur en <a>réalisant un don </a> ou <a class="aif-text-underline aif-text-underline--orange" href="mailto:smd@amnesty.fr">contacter le Service membres et donateurs</a> si vous pensez que c’est une erreur.';
+            aif_include_partial("alert", [
+                "title" => $title,
+            "content" => $content])
 
-                        Devenez donateur en <a>réalisant un don </a> ou <a class="aif-text-underline 
-aif-text-underline--orange" href="mailto:smd@amnesty.fr"> contatcer le
-                            Service membres et
-                            donateurs </a> si vous pensez
-                        que c’est une erreur.
+            ?>
 
-                    </p>
-
-                </div>
-            </div>
-        </div>
         <?php endif; ?>
 
         <form action="" method="POST" onsubmit="return checkPasswordMatch()">
