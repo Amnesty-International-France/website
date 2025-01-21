689c83ce2e819b9678b2eb188308fecb1b94d666
Volubyl <volubyl@pm.me>
2025-01-21 16:23:22 +0100

    feat: email autocomplete 2FA form (#31)

--- wp-content/plugins/aif-donor-space/templates/2FA-verification.php
+++ wp-content/plugins/aif-donor-space/templates/2FA-verification.php
@@ -7,56 +7,64 @@ $success_message = "";
 $disable_input = false;
 $error_message = "";
 $send_code_error_message = "";
+$email = "";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email']) && isset($_POST['2FA-code']) && isset($_POST['2FA_nonce'])) {
+if (!isset($_GET['ID'])) {
+    $error_title = "Une erreur est survenue";
+    $error_message = "Nous ne pouvons récupérer l'utilisateur associé à l'identifiant.";
 
-    $email = sanitize_text_field($_POST['email']);
+}
+
+if (isset($_GET['ID'])) {
+    $user = get_user_by("ID", $_GET['ID']);
+    if($user) {
+        $email = $user->user_email;
+    }
+
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['2FA-code']) && isset($_POST['2FA_nonce'])) {
+
     $code = sanitize_text_field($_POST['2FA-code']) ;
 
     if (!isset($_POST['2FA_nonce']) || !wp_verify_nonce($_POST['2FA_nonce'], '2FA_check')) {
         die('Invalid nonce.');
     }
 
+
     $user = get_user_by('email', $email);
-
-    if($user) {
-
+    if ($user) {
         $stored_code = get_2fa_code($user->ID);
-        if($stored_code &&  $stored_code === $code) {
+        if ($stored_code &&  $stored_code === $code) {
             store_email_is_verified($user->ID);
             reset_login_attempts($user->ID);
-            wp_redirect(get_permalink(get_page_by_path('espace-don/connectez-vous')));
+
+            $verification_url = add_query_arg([
+                "ID" => $user->ID,
+            ], get_permalink(get_page_by_path('espace-don/connectez-vous')));
+            wp_redirect($verification_url);
             exit;
         } else {
 
-            $disable_input = false;
-
-            if(limit_login_attempts($user->ID)) {
-                $error_message = "Le code ne correspond pas";
-
+            if (limit_login_attempts($user->ID)) {
+                $error_message = "Le code fourni est incorrect. Veuillez réessayer";
             } else {
-                $error_message = "La vérification du code à échoué. Veuillez recommencer dans quelques secondes";
+                $error_message = "Une erreur est survenue. Veuillez réessayer plus tard.";
 
             }
-
-
         }
 
     } else {
         $error_message = "Utilisateur inconnu ou code invalide";
 
     }
-
-
 }
+
 
 $send_code_error_message = "";
 $send_code_success_message = "";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['2FA-code-email']) && isset($_POST['2FA_new_code_nonce'])) {
-
-
-    $email = sanitize_text_field($_POST['2FA-code-email']);
+if ($_SERVER['REQUEST_METHOD'] === 'POST'  && isset($_POST['2FA_new_code_nonce'])) {
 
 
     if (!isset($_POST['2FA_new_code_nonce']) || !wp_verify_nonce($_POST['2FA_new_code_nonce'], '2FA_send_code')) {
@@ -79,12 +87,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['2FA-code-email']) && 
             $code = generate_2fa_code();
             store_2fa_code($stored_user->ID, $code);
 
-            $verification_url = get_permalink(get_page_by_path('espace-don/verifier-votre-email'));
+            $verification_url = add_query_arg([
+                "ID" => $user->ID,
+            ], get_permalink(get_page_by_path('espace-don/verifier-votre-email')));
+
             $message = 'Votre nouveau code est '. $code . '. Rendez-vous sur cette url vour activer votre compte: ' . ' '. $verification_url;
 
 
             if(send_2fa_code($email, $message)) {
-                $send_code_success_message = "Votre code est en chemin";
+                $send_code_success_message = "Nous avons bien reçu votre demande de code. Il arrivera dans votre boîte email d'ici quelques minutes";
             }
 
         }
@@ -127,23 +138,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['2FA-code-email']) && 
                 coordonnées personnelles, votre RIB et éditer des duplicatas de vos reçus fiscaux.</p>
 
             <p>
-                Pour finaliser ma création de compte, rentrer votre email et le code à 6 chiffres que vous venez de
-                recevoir
-                sur votre email.
+                Pour finaliser la création de votre compte, veuillez rentrer le code à 6 chiffres que vous venez de
+                recevoir par email
             </p>
 
-                <form class="aif-form-container" role="form" method="POST" action="">
+            <form class="aif-form-container" role="form" method="POST" action="">
                 <?php wp_nonce_field('2FA_check', '2FA_nonce'); ?>
-                <label class="<?php echo !empty($error_message) ? 'aif-input-error' : "" ?>" for="email">Votre
-                    adresse email
-                    (obligatoire)</label>
-                <input class="<?php echo !empty($error_message) ? "aif-input-error" : "" ?>" placeholder="" value=""
-                    type="email" name="email" id="email" autocomplete="email" required="true">
-                <label class="<?php echo !empty($error_message) ? "aif-input-error" : "" ?>" for="2FA-code">Code à 6
+                <label for="2FA-code">Code à 6
                     chiffres (obligatoire)</label>
-                <input pattern="\d{6}" title="rentrer ici votre code de 6 chiffres reçu par email"
-                    class="<?php echo !empty($error_message) ? "aif-input-error" : "" ?>" placeholder="" id=" 2FA-code"
-                    value="" type="text" name="2FA-code" required="true">
+                <input pattern="\d{6}" title="rentrer ici votre code de 6 chiffres reçu par email" placeholder=""
+                    id=" 2FA-code" value="" type="text" name="2FA-code" required="true">
 
                 <button class="btn aif-mt1w" type="submit">Valider la création de mon compte</button>
             </form>
@@ -156,54 +160,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['2FA-code-email']) && 
 
         <section class="aif-mb1w aif-mt1w">
 
-
-            <?php if (!empty($send_code_success_message)) : ?>
-            <div class="aif-bg-grey--lighter aif-p1w aif-mb1w">
-
-                <div class="aif-flex aif-gap-single">
-                    <div class="aif-text-green">
-                        <svg aria-hidden="true" width="4" height="14" viewBox="0 0 4 14" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M2 14C1.45 14 0.979167 13.8477 0.5875 13.5431C0.195833 13.2384 0 12.8722 0 12.4444C0 12.0167 0.195833 11.6505 0.5875 11.3458C0.979167 11.0412 1.45 10.8889 2 10.8889C2.55 10.8889 3.02083 11.0412 3.4125 11.3458C3.80417 11.6505 4 12.0167 4 12.4444C4 12.8722 3.80417 13.2384 3.4125 13.5431C3.02083 13.8477 2.55 14 2 14ZM0 9.33333V0H4V9.33333H0Z"
-                                fill="currentColor" />
-                        </svg>
-                    </div>
-                    <div>
-                        <p class="aif-text-green aif-text-bold">Votre code est en chemin</p>
-
-                        <p class="aif-mb0">
-                            Vous le receverez par email d'ici quelques minutes
-                        </p>
-
-                    </div>
-                </div>
-
-            </div>
-
-            <?php endif; ?>
-
-
-            <?php if (!empty($send_code_error_message)) : ?>
-            <?php
-            $title = "Une erreur est survenue";
-                aif_include_partial("alert", [
-                    "title" => $title,
-                "content" => $send_code_error_message])
-
-                ?>
-
-            <?php endif; ?>
             <h2> Code non reçu ou invalide ? </h2>
 
-                <form class="aif-form-container" role="form" method="POST" action="">
-                <?php wp_nonce_field('2FA_send_code', '2FA_new_code_nonce'); ?>
-                <label class="<?php echo !empty($error_message) ? 'aif-input-error' : "" ?>" for="2FA-code-email">Votre
-                    adresse email
-                    (obligatoire)</label>
-                <input class="<?php echo !empty($error_message) ? "aif-input-error" : "" ?>" placeholder="" value=""
-                    type="email" name="2FA-code-email" id="2FA-code-email" autocomplete="email" required="true">
+            <?php if (!empty($send_code_error_message)) {
+                $title = "Une erreur est survenue";
+                aif_include_partial("alert", [
+                    "title" => $title,
+                "content" => $send_code_error_message]);
 
+            }?>
+
+            <?php
+             if (!empty($send_code_success_message)) {
+                 aif_include_partial(
+                     "alert",
+                     [
+                     "title" => "Information",
+                     "content" => $send_code_success_message,
+                      "success" => true
+                      ]
+                 );
+             }?>
+
+            <form class="aif-form-container" role="form" method="POST" action="">
+                <?php wp_nonce_field('2FA_send_code', '2FA_new_code_nonce'); ?>
 
                 <button class="btn aif-mt1w" type="submit">Recevoir un nouveau code</button>
             </form>
