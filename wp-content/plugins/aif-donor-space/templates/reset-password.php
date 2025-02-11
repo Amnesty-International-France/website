f1dd6c3535ec7b0c67ba13179c369b5598fe030e
BertrandD. <volubyl@pm.me>
2025-02-11 11:38:42 +0100

    fix: forgotten password #4

--- wp-content/plugins/aif-donor-space/templates/reset-password.php
+++ wp-content/plugins/aif-donor-space/templates/reset-password.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['password']) && isset(
             if ($password == $_POST['confirm-password']) {
 
                 $userdata = [
-                    'ID'        => $ID,
+                    'ID'        => $user->ID,
                     'user_pass' => $password
                 ];
                 wp_update_user($userdata);
@@ -38,7 +38,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['password']) && isset(
 
 
                 wp_redirect($url);
-
                 exit;
 
             } else {
@@ -88,56 +87,64 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['password']) && isset(
 
     <section class="aif-container--form">
 
-    <h2>Modifier mon mot de passe</h2>
+        <h2>Modifier mon mot de passe</h2>
 
         <form class="aif-form-container" action="" method="POST">
 
-        <div class="aif-password-container">
-        <label class="aif-password-container__label" for="password">Nouveau mot de passe (obligatoire)</label>
-        <div class="aif-password-container__input-wrapper">
-            <input class="aif-password-container__input aif-input" name="password" aria-describedby="passwordHelp passphraseRequirements" type="password" id="password" autocomplete="new-password" required aria-required="true" oninput="checkPassphraseStrength()">
-            <button class="aif-password-container__button" type="button" id="toggle-password"  data-target="password" aria-label="Afficher ou masquer le mot de passe">
-                Afficher
-            </button>
-        </div>
-   
-        <?php
+            <div class="aif-password-container">
+                <label class="aif-password-container__label" for="password">Nouveau mot de passe (obligatoire)</label>
+                <div class="aif-password-container__input-wrapper">
+                    <input class="aif-password-container__input aif-input" name="password"
+                        aria-describedby="passwordHelp passphraseRequirements" type="password" id="password"
+                        autocomplete="new-password" required aria-required="true" oninput="checkPassphraseStrength()">
+                    <button class="aif-password-container__button" type="button" id="toggle-password"
+                        data-target="password" aria-label="Afficher ou masquer le mot de passe">
+                        Afficher
+                    </button>
+                </div>
+
+                <?php
          aif_include_partial("info-message", [
             "id" => "passwordHelp",
             "content" => "Exemple : Mon@MotDePasse123"]); ?>
 
-            
-            <div id="password-error-too-weak" class="aif-text-red aif-hide">
-                Le mot de passe est trop faible
-            </div>
 
-            <div id="passphraseRequirements">
-                <p class=" aif-m0 aif-mt1w ">Votre mot de passe doit </p>
-                <ul>
-                    <li id=" length">Doit contenir au moins 6 caractères</li>
-                    <li id="uppercase">Doit contenir au moins une lettre majuscule</li>
-                    <li id="lowercase">Doit contenir au moins une lettre minuscule</li>
-                    <li id="number">Doit contenir au moins un chiffre</li>
-                    <li id="special">Doit contenir au moins un caractère spécial (!, @, #, $, %,
-                        etc.)
-                    </li>
-                </ul>
-            </div>
+                <div id="password-error-too-weak" class="aif-text-red aif-hide">
+                    Le mot de passe est trop faible
+                </div>
 
-            <div class="aif-password-container">
-        <label class="aif-password-container__label" for="password">Nouveau mot de passe (obligatoire)</label>
-        <div class="aif-password-container__input-wrapper">
-            <input class="aif-password-container__input aif-input" name="confirm-password" aria-describedby="passwordHelp passphraseRequirements" type="password" id="confirm-password" autocomplete="new-password" required aria-required="true" oninput="checkPasswordMatch()">
-            <button class="aif-password-container__button" type="button" id="toggle-password"  data-target="confirm-password" aria-label="Afficher ou masquer le mot de passe">
-                Afficher
-            </button>
-        </div>
-            <div id="password-error-not-match" class="aif-text-red aif-hide">Les mots de passe ne
-                correspondent
-                pas.
-            </div>
+                <div id="passphraseRequirements">
+                    <p class=" aif-m0 aif-mt1w ">Votre mot de passe doit </p>
+                    <ul>
+                        <li id=" length">Doit contenir au moins 6 caractères</li>
+                        <li id="uppercase">Doit contenir au moins une lettre majuscule</li>
+                        <li id="lowercase">Doit contenir au moins une lettre minuscule</li>
+                        <li id="number">Doit contenir au moins un chiffre</li>
+                        <li id="special">Doit contenir au moins un caractère spécial (!, @, #, $, %,
+                            etc.)
+                        </li>
+                    </ul>
+                </div>
 
-            <button class="btn aif-mt1w aif-button--full" type="submit" id="submit-btn">Réinitialiser mon mot de passe</button>
+                <div class="aif-password-container">
+                    <label class="aif-password-container__label" for="password">Nouveau mot de passe
+                        (obligatoire)</label>
+                    <div class="aif-password-container__input-wrapper">
+                        <input class="aif-password-container__input aif-input" name="confirm-password"
+                            aria-describedby="passwordHelp passphraseRequirements" type="password" id="confirm-password"
+                            autocomplete="new-password" required aria-required="true" oninput="checkPasswordMatch()">
+                        <button class="aif-password-container__button" type="button" id="toggle-password"
+                            data-target="confirm-password" aria-label="Afficher ou masquer le mot de passe">
+                            Afficher
+                        </button>
+                    </div>
+                    <div id="password-error-not-match" class="aif-text-red aif-hide">Les mots de passe ne
+                        correspondent
+                        pas.
+                    </div>
+
+                    <button class="btn aif-mt1w aif-button--full" type="submit" id="submit-btn">Réinitialiser mon mot de
+                        passe</button>
 
 
         </form>
