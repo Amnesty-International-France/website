0ad1b3e12c51d7465f2e93e9cc19265144df01e5
Valentin Dassonville <valentin.dassonville@les-tilleuls.coop>
2025-02-21 18:02:51 +0100

    légende featured image + début design system

--- wp-content/themes/child-humanity-theme/functions.php
+++ wp-content/themes/child-humanity-theme/functions.php
@@ -1,20 +1,21 @@
 <?php
 
-if ( ! function_exists( 'article_styles' ) ) {
-    function article_styles() {
-        wp_enqueue_style( 'article', get_stylesheet_directory_uri() . '/assets/styles/article.css', [], wp_get_theme()->get( 'Version' ), 'all');
+if ( ! function_exists( 'front_assets_styles' ) ) {
+    function front_assets_styles() {
+        wp_enqueue_style( 'article', get_stylesheet_directory_uri() . '/assets/styles/article.css', [], wp_get_theme()->get( 'Version' ) );
+        wp_enqueue_style( 'header', get_stylesheet_directory_uri() . '/assets/styles/header.css', [], wp_get_theme()->get( 'Version' ) );
     }
 }
 
-add_action( 'wp_enqueue_scripts', 'article_styles' );
+add_action( 'wp_enqueue_scripts', 'front_assets_styles' );
 
-if ( ! function_exists( 'header_styles' ) ) {
-    function header_styles() {
-        wp_enqueue_style( 'header', get_stylesheet_directory_uri() . '/assets/styles/header.css', [], wp_get_theme()->get( 'Version' ), 'all' );
+if ( ! function_exists('editor_assets_styles') ) {
+    function editor_assets_styles() {
+        wp_enqueue_style( 'editor', get_stylesheet_directory_uri() . '/assets/styles/editor.css', [], wp_get_theme()->get( 'Version' ) );
     }
 }
 
-add_action( 'wp_enqueue_scripts', 'header_styles' );
+add_action( 'enqueue_block_editor_assets' , 'editor_assets_styles' );
 
 function calculate_reading_time() {
     $content = get_post_field( 'post_content', get_the_ID() );
@@ -31,10 +32,10 @@ if ( ! function_exists('amnesty_register_main_menu2') ) {
 }
 
 // Override le theme.json du plugin de branding par le theme.json du child theme
-function fusionner_theme_json_enfant_prioritaire( $theme_json ) {
+function override_theme_json( $theme_json ) {
     $enfant_theme_json = json_decode( file_get_contents( get_stylesheet_directory(). '/theme.json' ), true );
     $theme_json->update_with($enfant_theme_json);
     return $theme_json;
 }
 
-add_filter( 'wp_theme_json_data_theme', 'fusionner_theme_json_enfant_prioritaire', 100 );
+add_filter( 'wp_theme_json_data_theme', 'override_theme_json', 100 );
---
+++ wp-content/themes/child-humanity-theme/patterns/featured-image.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Title: Featured Image
+ * Description: Featured image output with caption, copyright, etc.
+ * Slug: amnesty/featured-image
+ * Inserter: yes
+ */
+
+use Amnesty\Get_Image_Data;
+
+if ( amnesty_post_has_hero() || amnesty_post_has_header() ) {
+	return;
+}
+
+if ( get_post_meta( get_the_ID(), '_hide_featured_image', true ) ) {
+	return;
+}
+
+$image_id = get_post_thumbnail_id( get_the_ID() );
+
+if ( ! $image_id ) {
+	return;
+}
+
+$image = new Get_Image_Data( $image_id );
+
+$attributes = [
+	'id'              => $image_id,
+	'className'       => 'article-figure is-stretched',
+	'sizeSlug'        => 'hero-md',
+	'linkDestination' => 'none',
+];
+
+?>
+<!-- wp:group {"tagName":"div","className":"container container--feature"} -->
+<div class="wp-block-group container container--feature">
+	<!-- wp:image <?php echo wp_kses_data( wp_json_encode( $attributes ) ); ?> -->
+	<figure class="wp-block-image <?php echo esc_attr( $attributes['className'] ); ?>">
+        <img src="<?php echo esc_url( amnesty_get_attachment_image_src( $image_id, 'hero-md' ) ); ?>" alt="" class="wp-image-<?php echo absint( $image_id ); ?>"/>
+    </figure>
+    <div class="feature-image-caption-container">
+        <div class="feature-image-caption-block">
+            <div class="feature-image-description">
+                <?php echo esc_html($image->credit()) ?>
+            </div>
+            <div class="feature-image-caption">
+                <?php echo esc_html($image->caption()) ?>
+            </div>
+        </div>
+    </div>
+	<!-- /wp:image -->
+</div>
+<!-- /wp:group -->
