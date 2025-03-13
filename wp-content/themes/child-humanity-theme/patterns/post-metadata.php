173318ebc2eb1660c9d56918ff4f9ebb829f6de4
Valentin Dassonville <valentin.dassonville@les-tilleuls.coop>
2025-03-13 16:14:38 +0100

    fix: space in article metadata

--- wp-content/themes/child-humanity-theme/patterns/post-metadata.php
+++ wp-content/themes/child-humanity-theme/patterns/post-metadata.php
@@ -46,7 +46,9 @@ $reading_time = calculate_reading_time();
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="metadata-icon">
                     <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clip-rule="evenodd" />
                 </svg>
-                Temps de lecture estimé : <?php echo esc_html( $reading_time === 1 ? "$reading_time minute" : "$reading_time minutes")?>
+                <div>
+                    Temps de lecture estimé : <?php echo esc_html( $reading_time === 1.0 ? "$reading_time minute" : "$reading_time minutes")?>
+                </div>
             </div>
         </div>
         <!-- /wp:group -->
--- wp-content/themes/child-humanity-theme/patterns/post-published-updated-date.php
+++ wp-content/themes/child-humanity-theme/patterns/post-published-updated-date.php
@@ -17,9 +17,11 @@ if ( $post_updated ) {
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="metadata-icon">
     <path fill-rule="evenodd" d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z" clip-rule="evenodd" />
 </svg>
-<?php do_action( 'amnesty_before_published_date' ); ?>
-Publié le <span class="publishedDate" aria-label="<?php /* translators: [front] */ esc_attr_e( 'Post published timestamp', 'amnesty' ); ?>"><time><?php echo esc_html( $post_published ); ?></time></span>
-<?php do_action( 'amnesty_after_published_date' ); ?>
-<?php if( $post_updated ): ?>
-    | Mis à jour le <span class="updatedDate" aria-label="<?php /* translators: [front] */ esc_attr_e( 'Post updated timestamp', 'amnesty' ); ?>"><time><?php echo esc_html( $post_updated ); ?></time></span>
-<?php endif ?>
+<div>
+    <?php do_action( 'amnesty_before_published_date' ); ?>
+    Publié le <span class="publishedDate" aria-label="<?php /* translators: [front] */ esc_attr_e( 'Post published timestamp', 'amnesty' ); ?>"><time><?php echo esc_html( $post_published ); ?></time></span>
+    <?php do_action( 'amnesty_after_published_date' ); ?>
+    <?php if( $post_updated ): ?>
+        | Mis à jour le <span class="updatedDate" aria-label="<?php /* translators: [front] */ esc_attr_e( 'Post updated timestamp', 'amnesty' ); ?>"><time><?php echo esc_html( $post_updated ); ?></time></span>
+    <?php endif ?>
+</div>
