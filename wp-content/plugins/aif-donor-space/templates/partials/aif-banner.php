5c15381121dd59abf4bb7334421d021217d6ac2a
BertrandD <volubyl@pm.me>
2025-03-13 11:34:00 +0100

    feat: improve aif banner

--- wp-content/plugins/aif-donor-space/templates/partials/aif-banner.php
+++ wp-content/plugins/aif-donor-space/templates/partials/aif-banner.php
@@ -4,83 +4,59 @@
     </div>
     <div class="aif-banner__container">
         
-    <?php if($member->isMembre && ($member->hasMandatActif || $member->isDonateur)): ?>
-        <h2 class="aif-banner__container__title"> Renforcez votre soutien </h2>
+    <?php if($member->hasMandatActif): ?>
+        <h2 class="aif-banner__container__title">  <?= $firstName ?>, renforcez votre soutien ! </h2>
         <p class="aif-banner__container__content">
 
-        <?= $firstName ?>, merci de nous soutenir. Vous souhaitez aller plus loin ?
+        Merci de nous soutenir ! Envie d’aller plus loin dans votre engagement ? 
         </p>
     
         <ul class="aif-banner__container__links">
      
             <li>
-                <a href="#" class="btn btn--primary">Faire un don ponctuel complémentaire</a>
+                <a href="https://soutenir.amnesty.fr/b?cid=427&lang=fr_FR" class="btn btn--primary">Je fais un don complémentaire</a>
             </li>        
      
         </ul>
     </div>
-    <?php endif ?>
 
-    <?php if($member->isMembre && !$member->hasMandatActif && !$member->isDonateur): ?>
-        <h2 class="aif-banner__container__title"> Renforcez votre soutien </h2>
+    <?php elseif($member->isMembre): ?>
+
+    <div>
+        <h2 class="aif-banner__container__title">  <?= $firstName ?>, renforcez votre soutien ! </h2>
         <p class="aif-banner__container__content">
 
-        <?= $firstName ?>, merci de nous soutenir. Vous souhaitez aller plus loin ?
+        Merci de nous soutenir ! Envie d’aller plus loin dans votre engagement ? 
         </p>
     
         <ul class="aif-banner__container__links">
      
             <li>
-                <a href="#" class="btn btn--primary">Faire un don ponctuel complémentaire</a>
+                <a href="https://soutenir.amnesty.fr/b?cid=428&lang=fr_FR" class="btn btn--primary">Faire un don ponctuel complémentaire</a>
             </li>
             
-            <li>
-                <a href="#" class="aif-banner__container__links__item">Passer en prélèvement automatique</a>
-            </li>  
      
         </ul>
     </div>
-    <?php endif ?>
- 
-    <?php if(!$member->isMembre && !$member->hasMandatActif && !$member->isDonateur): ?>
+    
+    <?php else: ?>
+
+    <div>
         <h2 class="aif-banner__container__title"> Agissez pour défendre les droits humains         </h2>
         <p class="aif-banner__container__content">
 
-        <?= $firstName ?>, merci de nous soutenir. Vous souhaitez aller plus loin ?
+            Merci de nous soutenir ! Envie d’aller plus loin dans votre engagement ? 
         </p>
     
         <ul class="aif-banner__container__links">
      
             <li>
-                <a href="#" class="btn btn--primary">Devenez membre</a>
+                <a href="https://soutenir.amnesty.fr/b?cid=429&lang=fr_FR" class="btn btn--primary">Je m’engage dans la durée. </a>
             </li>
-            
-            <li>
-                <a href="#" class="aif-banner__container__links__item">Faire un don ponctuel</a>
-            </li>  
      
         </ul>
     </div>
-    <?php endif ?> 
-
-    <?php if(!$member->isMembre && !$member->hasMandatActif && $member->isDonateur): ?>
-        <h2 class="aif-banner__container__title"> Agissez pour défendre les droits humains         </h2>
-        <p class="aif-banner__container__content">
-
-        <?= $firstName ?>, merci de nous soutenir. Vous souhaitez aller plus loin ?
-        </p>
     
-        <ul class="aif-banner__container__links">
-     
-            <li>
-                <a href="#" class="btn btn--primary">Devenez membre</a>
-            </li>
-            
-            <li>
-                <a href="#" class="aif-banner__container__links__item">Faire un don ponctuel</a>
-            </li>  
-     
-        </ul>
-    </div>
-    <?php endif ?> 
+    <?php endif ?>
+
 </div>
