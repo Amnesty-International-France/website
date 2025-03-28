8f66f9b4c310c51fc4f63466e6a33c70861c5a5a
Antoine Cau <antoine.cau@les-tilleuls.coop>
2025-03-28 16:24:51 +0100

    feat amn 28 key figure block

---
+++ wp-content/themes/humanity-theme/includes/blocks/key-figure/register.php
@@ -0,0 +1,18 @@
+<?php
+
+declare( strict_types = 1 );
+
+if (!function_exists('register_key_figure_block')) {
+	/**
+	 * Register the Key Figure block
+	 *
+	 * @package Amnesty\Blocks
+	 *
+	 * @return void
+	 */
+	function register_key_figure_block(): void {
+		register_block_type(
+			'amnesty-core/key-figure',
+		);
+	}
+}
