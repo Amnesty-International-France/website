b09e1caadb705c662b8a2b0f9a0d55af92af678b
Antoine Cau <antoine.cau@les-tilleuls.coop>
2025-04-09 15:09:17 +0200

    fix alignment and wrapper for chronicle page

--- wp-content/themes/humanity-theme/patterns/post-term-list-metadata.php
+++ wp-content/themes/humanity-theme/patterns/post-term-list-metadata.php
@@ -7,20 +7,25 @@
  * Inserter: no
  */
 
-$post_terms = wp_get_object_terms( get_the_ID(), get_object_taxonomies( get_post_type() ) );
-$main_category = amnesty_get_a_post_term( get_the_ID() );
+$post_terms = wp_get_object_terms(get_the_ID(), get_object_taxonomies(get_post_type()));
+$main_category = amnesty_get_a_post_term(get_the_ID());
 
-if ( empty( $post_terms ) || ! $main_category) {
-	return;
+if (empty($post_terms) || ! $main_category) {
+    return;
 }
 
 $post_terms = array_filter($post_terms, static function ($term) use ($main_category) {
     return $term->slug !== $main_category->slug;
 });
+
+$chip_style = match ($main_category->slug) {
+    'actualites', 'dossier' => 'bg-yellow',
+    'chroniques' => 'outline-yellow',
+    default => 'black-outline',
+};
+
 ?>
 
-<?php foreach ( $post_terms as $post_term ) : ?>
-
-<!-- wp:amnesty-core/chip-category {"label":"<?php echo esc_html( $post_term->name ); ?>","link":"<?php echo esc_url( amnesty_term_link( $post_term ) ); ?>","size":"medium","style":"bg-yellow"} /-->
-
+<?php foreach ($post_terms as $post_term) : ?>
+    <!-- wp:amnesty-core/chip-category {"label":"<?php echo esc_html($post_term->name); ?>","link":"<?php echo esc_url(amnesty_term_link($post_term)); ?>","size":"medium","style":"<?php echo esc_attr($chip_style); ?>"} /-->
 <?php endforeach; ?>
