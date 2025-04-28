a95fc0b8b1f13cf0f5da7db3671b836206442a78
Antoine Cau <antoine.cau@les-tilleuls.coop>
2025-04-28 15:52:32 +0200

    fix jp2 error

--- wp-content/plugins/prismic-migration/PrismicMigrationCli.php
+++ wp-content/plugins/prismic-migration/PrismicMigrationCli.php
@@ -10,15 +10,14 @@ class PrismicMigrationCli {
 		self::$dryrun = false;
 		$imported = 0;
 		try {
-			if( self::$dryrun ) {
+			if ( self::$dryrun ) {
 				WP_CLI::log('dry-mod activated');
 			}
-			//$prismic_docs = (new PrismicFetcher())->fetch_article("ZqF8vBEAACMALsnK");
 			$prismic_docs = (new PrismicFetcher())->fetch(type: Type::NEWS);
-
+	
 			$progress = WP_CLI\Utils\make_progress_bar( 'Importing documents', count($prismic_docs) );
 			foreach ( $prismic_docs as $doc ) {
-				if($this->post_exists_by_slug($doc['uid'])) {
+				if ( $this->post_exists_by_slug($doc['uid']) ) {
 					$progress->tick();
 					continue;
 				}
@@ -28,41 +27,54 @@ class PrismicMigrationCli {
 					WP_CLI::warning( $e->getMessage() );
 					continue;
 				}
+	
 				$wp_post = $transformer->parse( $doc );
-				if( !self::$dryrun ) {
-					$postId = wp_insert_post( $wp_post );
-					if( is_wp_error($postId) ) {
-						WP_CLI::warning( $postId->get_error_message() );
-						$progress->tick();
+	
+				if ( ! self::$dryrun ) {
+					try {
+						$postId = wp_insert_post( $wp_post );
+	
+						if ( is_wp_error($postId) ) {
+							WP_CLI::warning( 'Error inserting post: ' . $postId->get_error_message() );
+							$progress->tick();
+							continue;
+						}
+	
+						foreach ( $wp_post['terms'] as $term => $ids ) {
+							wp_set_object_terms( $postId, $ids, $term );
+						}
+	
+						$featured_image = $transformer->featuredImage( $doc );
+						if ( $featured_image !== false ) {
+							try {
+								$attachment_id = FileUploader::uploadMedia( $featured_image['imageUrl'], $featured_image['legend'], $featured_image['description'], $featured_image['alt'] );
+								set_post_thumbnail( $postId, $attachment_id );
+							} catch ( Exception $e ) {
+								WP_CLI::warning( 'Error during upload of the featured image: ' . $e->getMessage() );
+							}
+						}
+	
+						if ( isset($wp_post['relatedArticles']) ) {
+							update_post_meta( $postId, '_related_posts_selected', $wp_post['relatedArticles'] );
+						}
+					} catch ( Exception $e ) {
+						WP_CLI::warning( 'Error during post insertion (ID ' . $wp_post['ID'] . '): ' . $e->getMessage() );
 						continue;
 					}
-					foreach ( $wp_post['terms'] as $term => $ids ) {
-						wp_set_object_terms( $postId, $ids, $term );
-					}
-
-					$featured_image = $transformer->featuredImage( $doc );
-					if( $featured_image !== false) {
-						try {
-							$attachment_id = FileUploader::uploadMedia( $featured_image['imageUrl'], $featured_image['legend'], $featured_image['description'], $featured_image['alt']);
-							set_post_thumbnail( $postId, $attachment_id );
-						} catch ( Exception $e ) {
-							WP_CLI::warning( 'Error during upload of the featured image : ' . $e->getMessage() );
-						}
-					}
-
-					if( isset($wp_post['relatedArticles']) ) {
-						update_post_meta( $postId, '_related_posts_selected', $wp_post['relatedArticles'] );
-					}
 				}
+	
 				$imported++;
 				$progress->tick();
 			}
+	
 			$progress->finish();
 		} catch ( Exception $e ) {
 			WP_CLI::error( $e->getMessage() );
 		}
-		WP_CLI::success( 'Migration successful : ' . $imported . ' documents imported.' );
+	
+		WP_CLI::success( 'Migration successful: ' . $imported . ' documents imported.' );
 	}
+	
 
 	function post_exists_by_slug(string $slug): WP_Post|bool {
 		$query = new WP_Query([
