54234cca08159a38566f19b475f0aba059133cd9
Slim Amamou <slim@les-tilleuls.coop>
2025-05-06 14:21:54 +0100

    Fixed: was not getting database and site url base from env

--- infogerance/aif-clever-cloud.php
+++ infogerance/aif-clever-cloud.php
@@ -1,4 +1,11 @@
 <?php
+if (! empty(getenv('WP_HOME'))) {
+    define('WP_HOME', getenv('WP_HOME'));
+}
+if (! empty(getenv('WP_SITEURL'))) {
+    define('WP_SITEURL', getenv('WP_SITEURL'));
+}
+
 // auto-update
 define( 'WP_AUTO_UPDATE_CORE', true);
 
@@ -9,3 +16,16 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
     $_SERVER['HTTPS'] = 'on';
 }
 
+// database config from env variables
+if (! empty(getenv('MYSQL_ADDON_HOST')) && ! empty(getenv('MYSQL_ADDON_PORT'))) {
+    define('DB_HOST', getenv('MYSQL_ADDON_HOST').':'.getenv('MYSQL_ADDON_PORT'));
+}
+if (! empty(getenv('MYSQL_ADDON_DB'))) {
+    define('DB_NAME', getenv('MYSQL_ADDON_DB'));
+}
+if (! empty(getenv('MYSQL_ADDON_USER'))) {
+    define('DB_USER', getenv('MYSQL_ADDON_USER'));
+}
+if (! empty(getenv('MYSQL_ADDON_PASSWORD'))) {
+    define('DB_PASSWORD', getenv('MYSQL_ADDON_PASSWORD'));
+}
