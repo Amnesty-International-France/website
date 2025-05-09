f9d7ddd8c7399b5833eda61de4178bc9fe480c1b
Antoine Cau <antoine.cau@les-tilleuls.coop>
2025-05-09 13:56:26 +0200

    feat-amn57-landmarks-pages

--- wp-content/themes/humanity-theme/includes/taxonomies/class-taxonomy-combats.php
+++ wp-content/themes/humanity-theme/includes/taxonomies/class-taxonomy-combats.php
@@ -34,7 +34,7 @@ class Taxonomy_Combats extends Taxonomy {
 	 *
 	 * @var array
 	 */
-	protected $object_types = [ 'page', 'post' ];
+	protected $object_types = [ 'page', 'post', 'landmark' ];
 
 	/**
 	 * Taxonomy registration arguments
