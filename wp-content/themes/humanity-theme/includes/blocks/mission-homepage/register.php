d85fc8d0def77bcba8d669be98f190e1d777a2cd
Antoine Cau <antoine.cau@les-tilleuls.coop>
2025-06-18 17:55:20 +0200

    homepage mission block

---
+++ wp-content/themes/humanity-theme/includes/blocks/mission-homepage/register.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+if (!function_exists('register_mission_homepage_block')) {
+    /**
+     * Register the Mission Homepage block
+     */
+    function register_mission_homepage_block(): void {
+        register_block_type(
+			'amnesty-core/mission-homepage',
+			[
+				'render_callback' => 'render_mission_homepage_block',
+			]
+		);
+		
+    }
+}
