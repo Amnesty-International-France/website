99dbcbb4fc82f9a9213dedee3a4f603a1567e047
Maxime Urbanski <maxime@les-tilleuls.coop>
2025-06-24 09:58:37 +0200

    create template donation page and this style sheet
    wip
    
    # Conflicts:
    #	private/src/styles/pages/_events.scss
    #	wp-content/themes/humanity-theme/tribe-events/single-event.php

---
+++ wp-content/themes/humanity-theme/patterns/aside-donation-sticky.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Title: Aside Donation Sticky Pattern
+ * Description: Sticky aside block with image
+ * Slug: amnesty/aside-donation-sticky
+ * Inserter: no
+ */
+
+$image_url = get_template_directory_uri() . '/assets/images/testator-relations-officers.png';
+$icon_phone_url = get_template_directory_uri() . '/assets/images/icon-phone.svg';
+
+?>
+
+<aside class="page-donation-aside">
+
+</aside>
