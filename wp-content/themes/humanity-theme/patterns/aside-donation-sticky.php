cac5e2ebd499cf831a47913c9a0ad43f1b86c860
maxiloud <61539540+maxime-urbanski@users.noreply.github.com>
2025-07-03 09:28:50 +0200

    donation calculator sticky on donation page

--- wp-content/themes/humanity-theme/patterns/aside-donation-sticky.php
+++ wp-content/themes/humanity-theme/patterns/aside-donation-sticky.php
@@ -45,137 +45,46 @@ $amount_punctual_donation = [
 <aside class="page-donation-aside">
 	<div class="donation-calculator">
 		<div id="donation-header" class="donation-header">
+			<?php echo file_get_contents( get_template_directory() . '/assets/images/icon-don.svg' ); ?>
 			<div class="donation-title">
-				<?php echo file_get_contents( get_template_directory() . '/assets/images/icon-don.svg' ); ?>
 				<h4 class="subtitle">
 					Je calcule le montant de mon don
 					<span>après déduction fiscale de 66%</span>
 				</h4>
 			</div>
-			<!--<div class="donation-tabs">
-				<div id="punctual" class="punctual hidden">
-					Don Ponctuel
-				</div>
-				<div id="monthly" class="monthly active">
-					Don Mensuel
-				</div>
-			</div>-->
+
 		</div>
-		<div id="donation-body" class="donation-calculator-amount">
+		<div id="donation-body" class="donation-body">
 			<div class="give">
-				<div class="text">
-					<p>SI JE DONNE</p>
-				</div>
+				<h4 class="text">SI JE DONNE</h4>
 				<div class="input">
 					<input type="text"
 							id="input-donation"
 							class="input-donation"
-							value="0">
+							placeholder="0">
 					<label id="amount-total" for="input-donation">€<span>/mois</span></label>
 				</div>
 			</div>
 			<div class="cost">
-				<div class="text">
-					<p>CELA ME COÛTE</p>
+				<h4 class="text">CELA ME COÛTE</h4>
+				<div class="input yellow">
+					<input type="text"
+							id="input-donation-simulated"
+							class="input-donation-simulated"
+							value="0">
+					<label id="amount-total" for="input-donation">€<span>/mois</span></label>
 				</div>
 			</div>
-			<!--<div class="amount-punctual hidden">
-				<div id="amount-choices" class="radio-choices">
-					<?php /*foreach ( $amount_punctual_donation as $key => $value ) : */ ?>
-						<div class="don-radio
-						<?php
-						/*
-									if ( $value['checked'] ) :
-						*/
-						?>
-							active
-							<?php
-							/*
-									endif;
-							*/
-							?>
-						">
-							<label for="<?php /*echo esc_attr( $value['id'] ); */ ?>">
-								<?php /*echo esc_attr( $key ); */ ?>€
-							</label>
-							<input type="radio"
-									id="<?php /*echo esc_attr( $value['id'] ); */ ?>"
-								<?php
-								/*
-											if ( $value['checked'] ) :
-								*/
-								?>
-									checked
-									<?php
-									/*
-											endif;
-									*/
-									?>
-									value="<?php /*echo esc_attr( $key ); */ ?>">
-						</div>
-					<?php /*endforeach; */ ?>
-				</div>
-				<div class="amount-free">
-					<p>ou montant libre :</p>
-					<div class="input-container">
-						<input type="text" id="input-don-free-punctual" class="input-don-free"
-								placeholder="montant du don">
-						<label id="amount-total" for="input-don-free-punctual">€</label>
-					</div>
-				</div>
-			</div>
-			<div class="amount-monthly active">
-				<div id="amount-choices" class="radio-choices">
-					<?php /*foreach ( $amount_monthly_donation as $key => $value ) : */ ?>
-						<div class="don-radio
-						<?php
-						/*
-									if ( $value['checked'] ) :
-						*/
-						?>
-							active
-							<?php
-							/*
-									endif;
-							*/
-							?>
-						">
-							<label for="<?php /*echo esc_attr( $value['id'] ); */ ?>">
-								<?php /*echo esc_attr( $key ); */ ?>€
-							</label>
-							<input type="radio"
-									id="<?php /*echo esc_attr( $value['id'] ); */ ?>"
-								<?php
-								/*
-											if ( $value['checked'] ) :
-								*/
-								?>
-									checked
-									<?php
-									/*
-											endif;
-									*/
-									?>
-									value="<?php /*echo esc_attr( $key ); */ ?>">
-						</div>
-					<?php /*endforeach; */ ?>
-				</div>
-				<div class="amount-free">
-					<p>ou montant libre :</p>
-					<div class="input-container">
-						<input type="text" id="input-don-free-monthly" class="input-don-free"
-								placeholder="montant du don">
-						<label id="amount-total" for="input-don-free-monthly">€<span>/mois</span></label>
-					</div>
-				</div>
-			</div>-->
+
 		</div>
-		<div class="donation-calculator-simulation">
-			<p>Grâce à la réduction d'impôts de 66%, votre don ne vous coûtera que</p>
-			<p id="price-simulated" class="price-simulated"> €<span>/mois</span></p>
-			<a href="#" class="donation-link">
+		<div class="donation-calculator-footer">
+			<a href="https://soutenir.amnesty.fr/b"
+				target="_self"
+				class="donation-link">
 				<?php echo file_get_contents( get_template_directory() . '/assets/images/icon-arrow.svg' ); ?>
 				Faire un don
 			</a>
+			<p class="legend">Vous avez jusqu’au 31 décembre de l'année en cours pour bénéficier d’une réduction d’impôt
+				égale à 66% du montant de votre don.</p>
 		</div>
 </aside>
