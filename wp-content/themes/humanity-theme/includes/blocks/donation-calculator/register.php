6d8ea8b2cf9eb3c687223c949fc5cba5d3e13fcb
maxiloud <61539540+maxime-urbanski@users.noreply.github.com>
2025-07-04 09:47:55 +0200

    donation calculator block

---
+++ wp-content/themes/humanity-theme/includes/blocks/donation-calculator/register.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+if ( ! function_exists( 'register_donation_calculator_block' ) ) {
+	/**
+	 * Register Event Card Block
+	 *
+	 * @package Amnesty\Blocks
+	 *
+	 * @return void
+	 */
+	function register_donation_calculator_block(): void {
+		register_block_type(
+			'amnesty-core/donation-calculator',
+			[
+				'render_callback' => 'render_donation_calculator_block',
+				'attributes'      => [
+					'size'        => [
+						'type'    => 'string',
+						'default' => '',
+					],
+					'with_header' => [
+						'type'    => 'boolean',
+						'default' => false,
+					],
+					'with_tabs'   => [
+						'type'    => 'boolean',
+						'default' => false,
+					],
+					'with_legend' => [
+						'type'    => 'boolean',
+						'default' => false,
+					],
+				],
+			]
+		);
+	}
+}
