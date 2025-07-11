718ae6bae045c3e2e9cd20736064a5abeb502417
f-jost <frederic@les-tilleuls.coop>
2025-07-11 13:39:01 +0200

    WIP feat(structures): amn-226 - add custom page + map plugin

---
+++ wp-content/themes/humanity-theme/patterns/search-layout-with-map.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Title: Search Layout with Map
+ * Description: The left column is for searching and the right column for displaying results in a map
+ * Slug: amnesty/search-layout-with-map
+ * Inserter: yes
+ */
+
+?>
+
+<!-- wp:group {"tagName":"div", "className":"search-for-a-local-structure__container"} -->
+	<!-- wp:columns -->
+		<!-- wp:column {"width":"50%"} -->
+			<!-- wp:group {"className":"search-for-a-local-structure__search-bar"} -->
+				<!-- wp:paragraph {"placeholder":"Insérer la barre de recherche ici..."} -->
+				<p></p>
+				<!-- /wp:paragraph -->
+			<!-- /wp:group -->
+
+			<!-- wp:group {"className":"search-for-a-local-structure__list"} -->
+				<!-- wp:paragraph {"placeholder":"La liste des résultats s'affichera ici."} -->
+				<p></p>
+				<!-- /wp:paragraph -->
+			<!-- /wp:group -->
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"50%"} -->
+			<!-- wp:group {"className":"search-for-a-local-structure__map"} -->
+				<!-- wp:paragraph {"placeholder":"Ajouter le bloc 'Carte Interactive' ici."} -->
+				<p></p>
+				<!-- /wp:paragraph -->
+			<!-- /wp:group -->
+		<!-- /wp:column -->
+	<!-- /wp:columns -->
+<!-- /wp:group -->
