ebe38d8f9654e81e9b2b570bae3eb4aec53f3974
f-jost <frederic@les-tilleuls.coop>
2025-08-06 18:59:12 +0200

    feat(chronicle): #amn-310 - create promo page

---
+++ wp-content/themes/humanity-theme/patterns/content-callout.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Content callout
+ * Description: Outputs a block that "call" the reader's attention to key information.
+ * Slug: amnesty/content-callout
+ * Inserter: no
+ */
+
+$callout_title = get_field( 'callout_title', $post->ID ) ?? '';
+$callout_text = get_field( 'callout_text', $post->ID ) ?? '';
+
+?>
+
+<div class="content-callout">
+	<h3 class="content-callout__title"><?php echo esc_html($callout_title); ?></h3>
+	<p class="content-callout__text"><?php echo nl2br(esc_html($callout_text)); ?></p>
+</div>
