eefd9aa29c16564d63865c3bbb35f9db62d14ea1
Valentin Dassonville <valentin.dassonville59@gmail.com>
2025-08-13 16:54:23 +0200

    template par défaut mon espace

--- wp-content/db.php
+++
@@ -1,1 +0,0 @@
-/Users/antoinecau/dev/website/wp-content/plugins/query-monitor/wp-content/db.php
--- wp-content/themes/humanity-theme/includes/my-space/template.php
+++ wp-content/themes/humanity-theme/includes/my-space/template.php
@@ -14,7 +14,7 @@ function assign_myspace_template_to_descendants( $post_id, $post, $update ) {
     $ancestors = get_post_ancestors( $post_id );
 
     if ( ! empty($ancestors) && in_array( $parent_page_object->ID, $ancestors ) ) {
-        $template_file = 'templates/page-my-space-default.html';
+        $template_file = 'page-my-space-default';
 
         $current_template = get_page_template_slug( $post_id );
 
