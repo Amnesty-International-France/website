e4f8a36b5e32c82c2cb95ff23121bf3ff87dc462
Valentin Dassonville <129871973+valentin-dassonville@users.noreply.github.com>
2025-08-20 16:45:31 +0200

    feat : import edh (#332)

---
+++ wp-content/plugins/prismic-migration/transformers/EdhTransformer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace transformers;
+
+class EdhTransformer extends DocTransformer {
+
+	public function parse($prismicDoc): array {
+		$wp_post = parent::parse($prismicDoc);
+
+		$wp_post['post_type'] = \Type::get_wp_post_type(\Type::EDH);
+
+		$wp_post['meta_input']['theme'] = $prismicDoc['data']['accroche'];
+		$wp_post['meta_input']['_theme'] = 'field_689210a11445a';
+
+		return $wp_post;
+	}
+
+}
