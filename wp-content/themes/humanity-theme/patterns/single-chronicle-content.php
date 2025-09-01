811f3a5450103b9a7a783c8065d540d3a87a53ea
f-jost <frederic@les-tilleuls.coop>
2025-09-02 01:05:52 +0200

    feat(chronicle promo): #amn-391 - single chronicle integration

--- wp-content/themes/humanity-theme/patterns/single-chronicle-content.php
+++ wp-content/themes/humanity-theme/patterns/single-chronicle-content.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
  */
 
 $cover_image = get_field( 'cover_image' );
+$open_cover_image = get_field( 'cover_image_with_magazine_open' );
 $month = get_field( 'publication_month' );
 if ($month) {
 	$date_objet = DateTime::createFromFormat('!m', $month);
@@ -21,20 +22,31 @@ $summary_content = get_field('summary_content');
 
 ?>
 
+<?php
+if ( $open_cover_image ) : ?>
+<figure class="promo-open-magazine">
+	<img src="<?php echo esc_url( $open_cover_image['sizes']['medium_large'] ); ?>" alt="<?php echo esc_attr( $open_cover_image['alt'] ); ?>">
+</figure>
+<?php endif; ?>
+
 <!-- wp:group {"tagName":"section","className":"single-chronicle-content container has-gutter","layout":{"type":"constrained"}} -->
 <section class="wp-block-group single-chronicle__content container has-gutter">
 	<!-- wp:columns {"className":"columns"} -->
 		<!-- wp:column {"width":"33.33%"} -->
 		<aside class="single-chronicle__content__aside u-sticky">
 			<?php if ( $cover_image ) : ?>
-				<figure class="cover-image">
-					<img src="<?php echo esc_url( $cover_image['sizes']['medium_large'] ); ?>"
-						 alt="<?php echo esc_attr( $cover_image['alt'] ); ?>"
-					/>
-				</figure>
+			<figure class="cover-image">
+				<img src="<?php echo esc_url( $cover_image['sizes']['medium_large'] ); ?>"
+					 alt="<?php echo esc_attr( $cover_image['alt'] ); ?>"
+				/>
+			</figure>
 			<?php endif; ?>
+			<?php
+			echo do_blocks('
 			<!-- wp:amnesty-core/button {"label":"Abonnez-vous pour 3€/mois","size":"medium","icon":"arrow-right","linkType":"external","externalUrl":"https://soutenir.amnesty.fr/b?cid=365&lang=fr_FR&reserved_originecode=null","alignment":"center"} /-->
 			<!-- wp:amnesty-core/button {"label":"Voir les anciens numéros","size":"small","icon":"arrow-right","style":"bg-white outline-black","linkType":"internal","alignment":"center"} /-->
+			');
+			?>
 		</aside>
 		<!-- /wp:column -->
 
@@ -51,12 +63,20 @@ $summary_content = get_field('summary_content');
 
 <!-- wp:group {"tagName":"section","className":"container has-gutter","layout":{"type":"constrained"}} -->
 <section class="wp-block-group container has-gutter related-posts">
+	<?php
+	echo do_blocks('
 	<!-- wp:amnesty-core/related-posts {"title":"ET AUSSI", "nb_posts": 2, "display": "chronique"} /-->
+	');
+	?>
 </section>
 <!-- /wp:group -->
 
 <!-- wp:group {"tagName":"section","className":"container has-gutter","layout":{"type":"constrained"}} -->
 <section class="wp-block-group container has-gutter">
+	<?php
+	echo do_blocks('
 	<!-- wp:pattern {"slug":"amnesty/subscribe-to-the-chronicle-banner"} /-->
+	');
+	?>
 </section>
 <!-- /wp:group -->
