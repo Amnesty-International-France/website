4a12c275c68fb639e1c6bf52430b24f0c6493c1b
f-jost <frederic@les-tilleuls.coop>
2025-09-02 01:05:52 +0200

    feat(chronicle promo): #amn-391 - single chronicle integration

---
+++ wp-content/themes/humanity-theme/includes/blocks/latest-chronicle-promo/register.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+if ( ! function_exists( 'register_latest_chronicle_promo_block' ) ) {
+	/**
+	 * Register the "Latest Chronicle Promo" block.
+	 * This block finds and displays the latest chronicle post.
+	 *
+	 * @package Amnesty\Blocks
+	 *
+	 * @return void
+	 */
+	function register_latest_chronicle_promo_block(): void {
+		register_block_type( 'amnesty/latest-chronicle-promo', [
+			'render_callback' => 'render_latest_chronicle_promo',
+			'attributes'      => [],
+		]);
+	}
+}
+
+add_action( 'init', 'register_latest_chronicle_promo_block' );
---
+++ wp-content/themes/humanity-theme/includes/blocks/latest-chronicle-promo/render.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+if ( ! function_exists( 'render_latest_chronicle_promo' ) ) {
+	/**
+	 * Render callback for the "amnesty/latest-chronicle-promo" block.
+	 * Finds the latest chronicle post and displays its content using another pattern.
+	 *
+	 * @return string The HTML output of the block.
+	 */
+	function render_latest_chronicle_promo(): string {
+		$args = [
+			'post_type'      => 'chronique',
+			'posts_per_page' => 1,
+			'orderby'        => 'date',
+			'order'          => 'DESC',
+			'post_status'    => 'publish',
+		];
+
+		$latest_chronicle_query = new WP_Query( $args );
+
+		ob_start();
+
+		if ( $latest_chronicle_query->have_posts() ) {
+			while ( $latest_chronicle_query->have_posts() ) {
+				$latest_chronicle_query->the_post();
+
+				get_template_part( 'patterns/single-chronicle-content' );
+			}
+		} else {
+			echo '<p>Aucun numéro de chronique à afficher.</p>';
+		}
+
+		wp_reset_postdata();
+
+		return ob_get_clean();
+	}
+}
