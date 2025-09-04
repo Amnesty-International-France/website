c59a7e4fd5886117d9df982c2206fc1760826fcc
Maxiloud <maxime@les-tilleuls.coop>
2025-09-04 11:43:39 +0200

    Fix - EDH card & single page
    * added content type on card
    * new template for single page

---
+++ wp-content/themes/humanity-theme/patterns/archive-loop-edh.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Title: Archive loop for EDH
+ * Description: Template for the loop on archive edh page
+ * Slug: amnesty/archive-loop-edh
+ * Inserter: yes
+ */
+
+declare(strict_types=1);
+
+add_filter( 'get_the_terms', 'amnesty_limit_post_terms_results_for_archive' );
+
+?>
+
+<!-- wp:query {"inherit":true} -->
+<div class="wp-block-query">
+	<!-- wp:group {"tagName":"div","className":""} -->
+	<div class="wp-block-group news-section section section--small section--tinted has-gutter">
+		<!-- wp:group {"tagName":"div","className":"postlist"} -->
+		<div class="wp-block-group postlist">
+			<!-- wp:post-template {"layout":{"type":"grid","columnCount":3},"className":"post-grid"} -->
+			<!-- wp:amnesty-core/edh-card {"direction":"portrait"} /-->
+			<!-- /wp:post-template -->
+
+			<!-- wp:query-no-results -->
+			<div class="wp-block-query-no-results">
+				<p>Nous n’avons pas trouvé d’articles correspondant à vos critères de recherche.</p>
+			</div>
+			<!-- /wp:query-no-results -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+
+	<!-- wp:query-pagination {"align":"center","className":"section section--small","paginationArrow":"none","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"nowrap"}} -->
+	<!-- wp:query-pagination-previous {"label":"<?php echo esc_html( __( 'Previous', 'amnesty' ) ); ?>"} /-->
+	<!-- wp:query-pagination-numbers {"midSize":1,"className":"page-numbers"} /-->
+	<!-- wp:query-pagination-next {"label":"<?php echo esc_html( __( 'Next', 'amnesty' ) ); ?>"} /-->
+	<!-- /wp:query-pagination -->
+</div>
+<!-- /wp:query -->
