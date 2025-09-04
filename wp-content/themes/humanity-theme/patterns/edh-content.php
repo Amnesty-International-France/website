2fa7984dade32b7eb6bce2630533cb1a6d37db4b
Maxiloud <maxime@les-tilleuls.coop>
2025-09-04 11:43:39 +0200

    Fix - EDH card & single page
    * added content type on card
    * new template for single page

--- wp-content/themes/humanity-theme/patterns/edh-content.php
+++ wp-content/themes/humanity-theme/patterns/edh-content.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * Title: EDH Content
  * Description: Output the content of a single edh post
@@ -9,36 +7,16 @@ declare(strict_types=1);
  * Inserter: no
  */
 
+declare(strict_types=1);
+
 ?>
 
 <!-- wp:group {"tagName":"section","className":"edh"} -->
-<section class="wp-block-group edh-single">
-	<!-- wp:group {"tagName":"header","className":"fo-header"} -->
-	<header class="wp-block-group edh-header">
-		<div class="yoast-breadcrumb-wrapper">
-			<?php if (function_exists('yoast_breadcrumb')) {
-				yoast_breadcrumb('<nav class="yoast-breadcrumb">', '</nav>');
-			} ?>
-		</div>
-		<!-- wp:group {"className":"files"} -->
-		<!-- wp:pattern {"slug":"amnesty/post-edh-metadata"} /-->
-
-		<!-- /wp:group -->
-	</header>
-	<!-- /wp:group -->
-
+<article class="wp-block-group edh-single">
 	<!-- wp:group {"tagName":"edh","className":"edh-content"} -->
-	<article class="wp-block-group edh-content">
-		<!-- wp:pattern {"slug":"amnesty/featured-image"} /-->
+	<section class="wp-block-group edh-content">
 		<!-- wp:post-content /-->
-
-		<!-- wp:group {"tagName":"footer","className":"article-footer"} -->
-		<footer class="wp-block-group article-footer">
-			<!-- wp:pattern {"slug":"amnesty/post-terms"} /-->
-		</footer>
-		<!-- /wp:group -->
-	</article>
+	</section>
 	<!-- /wp:group -->
-
-</section>
+</article>
 <!-- /wp:group -->
