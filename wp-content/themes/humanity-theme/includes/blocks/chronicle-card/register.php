e0dd7c96e306753566c11f157902b785d6d845c2
f-jost <frederic@les-tilleuls.coop>
2025-09-05 10:41:31 +0200

    feat(chronicle): #amn-312 - archive page

---
+++ wp-content/themes/humanity-theme/includes/blocks/chronicle-card/register.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+if (!function_exists('register_chronicle_card_block')) {
+	function register_chronicle_card_block(): void
+	{
+		register_block_type(
+			'amnesty-core/chronicle-card',
+			[
+				'render_callback' => 'render_chronicle_card_block',
+			]
+		);
+	}
+}
