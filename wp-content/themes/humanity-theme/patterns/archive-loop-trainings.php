40c7ab0a459326cbe58dabf6ce9a5402afb6aa98
Maxiloud <maxime@les-tilleuls.coop>
2025-09-09 09:58:51 +0200

    fix training card

--- wp-content/themes/humanity-theme/patterns/archive-loop-trainings.php
+++ wp-content/themes/humanity-theme/patterns/archive-loop-trainings.php
@@ -11,23 +11,23 @@ add_filter('get_the_terms', 'amnesty_limit_post_terms_results_for_archive');
 
 <!-- wp:query {"inherit":true} -->
 <div class="wp-block-query">
-    <!-- wp:group {"tagName":"div","className":""} -->
-    <div class="wp-block-group news-section section section--small section--tinted has-gutter">
-        <!-- wp:group {"tagName":"div","className":"postlist"} -->
-        <div class="wp-block-group postlist">
-            <!-- wp:post-template {"layout":{"type":"grid","columnCount":3},"className":"post-grid"} -->
-                <!-- wp:amnesty-core/edh-card {"direction":"portrait"} /-->
-            <!-- /wp:post-template -->
+	<!-- wp:group {"tagName":"div","className":""} -->
+	<div class="wp-block-group news-section section section--small section--tinted has-gutter">
+		<!-- wp:group {"tagName":"div","className":"postlist"} -->
+		<div class="wp-block-group postlist">
+			<!-- wp:post-template {"layout":{"type":"grid","columnCount":3},"className":"post-grid"} -->
+				<!-- wp:amnesty-core/training-card {"direction":"portrait"} /-->
+			<!-- /wp:post-template -->
 
-            <!-- wp:query-no-results -->
-            <div class="wp-block-query-no-results">
-                <p>Nous n’avons pas trouvé d’articles correspondant à vos critères de recherche.</p>
-            </div>
-            <!-- /wp:query-no-results -->
-        </div>
-        <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
+			<!-- wp:query-no-results -->
+			<div class="wp-block-query-no-results">
+				<p>Nous n’avons pas trouvé d’articles correspondant à vos critères de recherche.</p>
+			</div>
+			<!-- /wp:query-no-results -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
 
     <!-- wp:query-pagination {"align":"center","className":"section section--small","paginationArrow":"none","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"nowrap"}} -->
         <!-- wp:query-pagination-previous {"label":"<?php echo esc_html(__('Previous', 'amnesty')); ?>"} /-->
