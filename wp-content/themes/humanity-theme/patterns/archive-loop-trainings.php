f4126994992c726513b16221cb2fef8a6e4cc05f
Maxiloud <maxime@les-tilleuls.coop>
2025-09-09 09:58:51 +0200

    fix training card

--- wp-content/themes/humanity-theme/patterns/archive-loop-trainings.php
+++ wp-content/themes/humanity-theme/patterns/archive-loop-trainings.php
@@ -19,15 +19,15 @@ add_filter('get_the_terms', 'amnesty_limit_post_terms_results_for_archive');
                 <!-- wp:amnesty-core/training-card {"direction":"portrait"} /-->
             <!-- /wp:post-template -->
 
-            <!-- wp:query-no-results -->
-            <div class="wp-block-query-no-results">
-                <p>Nous n’avons pas trouvé d’articles correspondant à vos critères de recherche.</p>
-            </div>
-            <!-- /wp:query-no-results -->
-        </div>
-        <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
+			<!-- wp:query-no-results -->
+			<div class="wp-block-query-no-results">
+				<p>Nous n’avons pas trouvé d’articles correspondant à vos critères de recherche.</p>
+			</div>
+			<!-- /wp:query-no-results -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
 
     <!-- wp:query-pagination {"align":"center","className":"section section--small","paginationArrow":"none","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"nowrap"}} -->
         <!-- wp:query-pagination-previous {"label":"<?php echo esc_html(__('Previous', 'amnesty')); ?>"} /-->
