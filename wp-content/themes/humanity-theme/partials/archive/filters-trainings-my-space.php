ce6883d28cd1a90060a34b3096b13f6d02040938
Antoine Cau <antoine.cau@les-tilleuls.coop>
2025-09-12 13:40:43 +0200

    trainings list in my space

---
+++ wp-content/themes/humanity-theme/partials/archive/filters-trainings-my-space.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Archives partial, trainings filters (wrapper)
+ *
+ * @package Amnesty\Partials
+ */
+
+global $wp;
+
+$form_url = home_url(add_query_arg([], $wp->request));
+
+?>
+
+<section class="postlist-categoriesContainer" data-slider>
+    <form id="filter-form" class="trainings-filters" action="<?php echo esc_url($form_url); ?>" method="get">
+        <?php require locate_template('partials/forms/trainings-filters-my-space.php'); ?>
+    </form>
+</section>
