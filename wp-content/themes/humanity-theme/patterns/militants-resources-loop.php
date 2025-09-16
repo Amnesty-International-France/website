a518ddc9a2ecdfa072abf780b6e65c8c16a9a59c
Mehdi Zaidi <mehdi@les-tilleuls.coop>
2025-09-16 14:37:42 +0200

    feat: integration of militant resources page on my space

--- wp-content/themes/humanity-theme/patterns/militants-resources-loop.php
+++ wp-content/themes/humanity-theme/patterns/militants-resources-loop.php
@@ -19,9 +19,12 @@ $meta_query = [
     ],
 ];
 
+$search = sanitize_text_field($_GET['q']) ?? null;
+$posts_per_page = $search ? -1 : 2;
+
 $args = [
     'post_type' => 'document',
-    'posts_per_page' => 2,
+    'posts_per_page' => $posts_per_page,
     'paged' => $paged,
     'meta_query' => $meta_query,
 ];
@@ -46,7 +49,6 @@ if ($type_filter) {
 
 $args['tax_query'] = $tax_query;
 
-$search = sanitize_text_field($_GET['q']) ?? null;
 if ($search) {
     $args['s'] = $search;
     unset($args['paged']);
@@ -54,23 +56,9 @@ if ($search) {
 
 $query = new WP_Query($args);
 ?>
-<form action="<?php echo esc_url($form_url); ?>">
-	<input type="text" name="q" id="search-ressource" value="<?php echo esc_attr(get_search_query()); ?>" placeholder="Rechercher une ressource...">
-	<div id="results-ressource"></div>
-	<button type="submit">Rechercher</button>
-</form>
 <?php if ($query->have_posts()) : ?>
 	<?php while ($query->have_posts()) : $query->the_post(); ?>
-		<?php
-        $block = [
-            'blockName'    => 'amnesty-core/article-card',
-            'attrs'        => ['direction' => 'portrait'],
-            'innerBlocks'  => [],
-            'innerHTML'    => '',
-            'innerContent' => [],
-        ];
-	    echo render_block($block);
-	    ?>
+		<?php require locate_template('partials/document/card.php'); ?>
 	<?php endwhile; ?>
 <?php else : ?>
 	<div class="wp-block-query-no-results">
@@ -97,37 +85,3 @@ if ($pagination) {
 
 wp_reset_postdata();
 ?>
-
-<script>
-	document.addEventListener("DOMContentLoaded", function () {
-		const input = document.getElementById("search-ressource");
-		const results = document.getElementById("results-ressource");
-
-		input.addEventListener("keyup", function () {
-			let searchTerm = this.value.trim();
-
-			if (searchTerm.length < 2) {
-				results.innerHTML = "";
-				return;
-			}
-
-			fetch(`/wp-json/humanity/v1/search-document-militant?term=${encodeURIComponent(searchTerm)}`)
-				.then(res => res.json())
-				.then(data => {
-					results.innerHTML = "";
-
-					if (data.length > 0) {
-						let ul = document.createElement("ul");
-						data.forEach(item => {
-							let li = document.createElement("li");
-							li.innerHTML = `<a href="${window.location.href.split('?')[0]}?q=${item.title}">${item.title}</a>`;
-							ul.appendChild(li);
-						});
-						results.appendChild(ul);
-					} else {
-						results.innerHTML = "<p>Aucun résultat</p>";
-					}
-				});
-		});
-	});
-</script>
