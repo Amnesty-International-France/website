66d7b5df8040ccc0efcd20d649befe894ea04290
Antoine Cau <antoine.cau@les-tilleuls.coop>
2025-09-19 09:22:28 +0200

    single new for my space

---
+++ wp-content/themes/humanity-theme/patterns/single-actuality-my-space.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template pour afficher une formation single dans le contexte "Mon Espace"
+ */
+
+?>
+
+<?php get_header(); ?>
+
+<div class="aif-donor-space-layout">
+    <?php echo render_block(['blockName' => 'core/pattern', 'attrs' => ['slug' => 'amnesty/my-space-sidebar'] ]); ?>
+    <main class="aif-donor-space-content">
+        <?php echo render_block(['blockName' => 'core/pattern', 'attrs' => ['slug' => 'amnesty/my-space-header'] ]); ?>
+        <section class="wp-block-group single-actuality-my-space article actualites">
+            <header class="wp-block-group article-header">
+                <?php echo render_block(['blockName' => 'core/pattern', 'attrs' => ['slug' => 'amnesty/post-metadata'] ]); ?>
+            </header>
+            <article class="wp-block-group training-content">
+                <?php echo render_block(['blockName' => 'core/pattern', 'attrs' => ['slug' => 'amnesty/featured-image'] ]); ?>
+                <?php echo render_block(['blockName' => 'core/post-content']); ?>
+                <footer class="wp-block-group article-footer">
+                    <?php echo render_block(parsed_block: ['blockName' => 'core/pattern', 'attrs' => ['slug' => 'amnesty/post-terms'] ]); ?>
+                </footer>
+            </article>
+        </section>
+    </main>
+</div>
+
+<?php get_footer(); ?>
