c5ab274ae10198e25414b93afc0ded8befe695e7
Antoine Cau <antoine.cau@les-tilleuls.coop>
2025-09-23 08:17:56 +0200

    defaut press release img in archive page

--- wp-content/themes/humanity-theme/partials/article-card.php
+++ wp-content/themes/humanity-theme/partials/article-card.php
@@ -27,6 +27,12 @@ if (!$post_object instanceof WP_Post) {
     $date = get_the_date('', $post_object);
     $thumbnail = get_the_post_thumbnail($post_id, 'medium', ['class' => 'article-image']);
 
+    if ('press-release' === get_post_type($post_object) && !$thumbnail) {
+        $default_image_url = get_template_directory_uri() . '/assets/images/default-press-release-small.png';
+        $thumbnail = '<img src="' . esc_url($default_image_url) . '" alt="' . esc_attr($title) . '" class="article-image">';
+    }
+
+
     $main_category = amnesty_get_a_post_term($post_id);
     if (!($main_category instanceof WP_Term)) {
         $main_category = null;
@@ -90,59 +96,59 @@ if (!$post_object instanceof WP_Post) {
 ?>
 
 <article class="article-card card-<?php echo esc_attr($direction); ?>">
-	<?php if ($thumbnail) : ?>
-		<a href="<?= esc_url($permalink); ?>" class="article-thumbnail">
-			<?= $thumbnail; ?>
-		</a>
-	<?php else : ?>
-		<div class="article-thumbnail"></div>
-	<?php endif; ?>
+    <?php if ($thumbnail) : ?>
+        <a href="<?= esc_url($permalink); ?>" class="article-thumbnail">
+            <?= $thumbnail; ?>
+        </a>
+    <?php else : ?>
+        <div class="article-thumbnail"></div>
+    <?php endif; ?>
 
-	<?php if (!empty($label) && get_post_type($post_object) !== 'page'): ?>
-		<?= render_chip_category_block([
-		    'label' => esc_html($label),
-		    'link' => esc_url($link),
-		    'size' => 'large',
-		    'style' => esc_attr($chip_style),
-		    'isLandmark' => ('landmark' === get_post_type($post_object)),
-		    'icon' => $icon ?? '',
-		]); ?>
-	<?php endif; ?>
+    <?php if (!empty($label) && get_post_type($post_object) !== 'page'): ?>
+        <?= render_chip_category_block([
+            'label' => esc_html($label),
+            'link' => esc_url($link),
+            'size' => 'large',
+            'style' => esc_attr($chip_style),
+            'isLandmark' => ('landmark' === get_post_type($post_object)),
+            'icon' => $icon ?? '',
+        ]); ?>
+    <?php endif; ?>
 
-	<div class="article-content">
-		<time class="article-date" datetime="<?= esc_attr(date('c', strtotime($date))); ?>">
-			<?= esc_html($date); ?>
-		</time>
-		<div class="article-title">
-			<a class="as-h5" href="<?= esc_url($permalink); ?>">
-				<?= esc_html($title); ?>
-			</a>
-		</div>
-		<div class="article-terms <?php if (empty($post_terms)) {
-		    echo 'is-empty';
-		} ?>">
-			<?php foreach ($post_terms as $term): ?>
-				<?php
-		        $term_link = get_term_link($term);
+    <div class="article-content">
+        <time class="article-date" datetime="<?= esc_attr(date('c', strtotime($date))); ?>">
+            <?= esc_html($date); ?>
+        </time>
+        <div class="article-title">
+            <a class="as-h5" href="<?= esc_url($permalink); ?>">
+                <?= esc_html($title); ?>
+            </a>
+        </div>
+        <div class="article-terms <?php if (empty($post_terms)) {
+            echo 'is-empty';
+        } ?>">
+            <?php foreach ($post_terms as $term): ?>
+                <?php
+                $term_link = get_term_link($term);
 
-			    if (!is_wp_error($term_link)) {
-			        $custom_routes = [
-			            'combat'   => '/combats/',
-			            'location' => '/pays/',
-			        ];
+                if (!is_wp_error($term_link)) {
+                    $custom_routes = [
+                        'combat'   => '/combats/',
+                        'location' => '/pays/',
+                    ];
 
-			        if (array_key_exists($term->taxonomy, $custom_routes)) {
-			            $term_link = $custom_routes[$term->taxonomy] . $term->slug;
-			        }
-			    }
-			    ?>
-				<?= render_chip_category_block([
-				    'label' => esc_html($term->name),
-				    'size'  => 'small',
-				    'style' => 'bg-gray',
-				    'link'  => esc_url($term_link),
-				]) ?>
-			<?php endforeach; ?>
-		</div>
-	</div>
+                    if (array_key_exists($term->taxonomy, $custom_routes)) {
+                        $term_link = $custom_routes[$term->taxonomy] . $term->slug;
+                    }
+                }
+                ?>
+                <?= render_chip_category_block([
+                    'label' => esc_html($term->name),
+                    'size'  => 'small',
+                    'style' => 'bg-gray',
+                    'link'  => esc_url($term_link),
+                ]) ?>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </article>
