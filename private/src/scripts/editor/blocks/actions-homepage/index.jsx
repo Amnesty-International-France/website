f62101f3622f40944a0116d14a0a0981536228c0
f-jost <frederic@les-tilleuls.coop>
2025-09-29 18:33:30 +0200

    feat: add actions-homepage block preview

--- private/src/scripts/editor/blocks/actions-homepage/index.jsx
+++ private/src/scripts/editor/blocks/actions-homepage/index.jsx
@@ -6,6 +6,7 @@ registerBlockType('amnesty-core/actions-homepage', {
   title: 'Actions Homepage',
   description: "Section pour l'affichage d'actions sur la page d'accueil",
   category: 'amnesty-core',
+  icon: 'grid-view',
   attributes: {
     title: {
       type: 'string',
@@ -38,6 +39,39 @@ registerBlockType('amnesty-core/actions-homepage', {
           buttonLabel: 'Label du bouton',
           buttonLink: '',
           imageUrl: '',
+        },
+      ],
+    },
+  },
+  example: {
+    attributes: {
+      title: 'Nos combats actuels',
+      chapo:
+        'De la défense des droits humains à la lutte contre la discrimination, découvrez comment vous pouvez faire la différence dès aujourd’hui.',
+      items: [
+        {
+          itemTitle: "Liberté d'expression",
+          itemDescription:
+            'Partout dans le monde, des voix sont réduites au silence. Aidez-nous à protéger les journalistes et les activistes.',
+          buttonLabel: 'Défendre ce droit',
+          buttonLink: '#',
+          imageUrl: 'https://placehold.co/600x400/222222/FFFFFF/png?text=Liberté',
+        },
+        {
+          itemTitle: 'Contre la torture',
+          itemDescription:
+            'La torture est une pratique barbare qui doit cesser. Signez nos pétitions pour exiger la justice pour les victimes.',
+          buttonLabel: 'Exiger la justice',
+          buttonLink: '#',
+          imageUrl: 'https://placehold.co/600x400/ffdb00/000000/png?text=Justice',
+        },
+        {
+          itemTitle: 'Droits des réfugiés',
+          itemDescription:
+            'Des millions de personnes sont forcées de fuir leur foyer. Agissez pour garantir leur sécurité et leur dignité.',
+          buttonLabel: 'Agir pour eux',
+          buttonLink: '#',
+          imageUrl: 'https://placehold.co/600x400/555555/FFFFFF/png?text=Solidarité',
         },
       ],
     },
