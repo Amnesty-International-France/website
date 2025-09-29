68ddbee69668b25ee51ba988133d13784728b66b
Antoine Cau <antoine.cau@les-tilleuls.coop>
2025-09-29 17:05:41 +0200

    404 page

--- wp-content/themes/humanity-theme/patterns/404.php
+++ wp-content/themes/humanity-theme/patterns/404.php
@@ -1,23 +1,21 @@
 <?php
 /**
  * Title: 404 Page Content
- * Description: Translatable content for the 404 page.
+ * Description: Content for the 404 page.
  * Slug: amnesty/404
  * Inserter: no
  */
 ?>
 
-<!-- wp:heading {"textAlign":"center","level":1} -->
-<h1 class="wp-block-heading has-text-align-center"><?php echo esc_html( /* translators: [front] https://wordpresstheme.amnesty.org/the-theme/templates/t006-404/  */ __('Page Not Found', 'amnesty')); ?></h1>
-<!-- /wp:heading -->
-
-<!-- wp:heading {"textAlign":"center","level":3} -->
-<h3 class="wp-block-heading has-text-align-center"><?php esc_html_e('The page you are looking for does not exist.', 'amnesty'); ?></h3>
-<!-- /wp:heading -->
-
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="<?php echo esc_url(get_home_url()); ?>"><?php /* translators: [front] https://wordpresstheme.amnesty.org/the-theme/templates/t006-404/ */ esc_html_e('Go to the Homepage', 'amnesty'); ?></a></div>
-<!-- /wp:button -->
+<div class="page-404-content">
+    <!-- wp:amnesty-core/chip-category {"label":"Erreur 404", "size":"large","style":"bg-black"} /-->
+    <h1 class="wp-block-heading page-404-content-title">Page introuvable</h1>
+    <h3 class="wp-block-heading page-404-content-subtitle">Il semble que la page que vous tentez d'afficher n'existe pas.</h3>
+    <div class='custom-button-block center'>
+        <a href="<?php home_url() ?>" target="_blank" rel="noopener noreferrer" class="custom-button">
+            <div class='content bg-yellow medium'>
+                <div class="button-label">Retourner à l'accueil</div>
+            </div>
+        </a>
+    </div>
 </div>
-<!-- /wp:buttons -->
