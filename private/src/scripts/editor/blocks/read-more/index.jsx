73e44c7df84c4e4f7564ab45ad564fea93480171
f-jost <frederic@les-tilleuls.coop>
2025-10-01 01:04:49 +0200

    feat: add read-more block preview

--- private/src/scripts/editor/blocks/read-more/index.jsx
+++ private/src/scripts/editor/blocks/read-more/index.jsx
@@ -8,7 +8,22 @@ registerBlockType('amnesty-core/read-more', {
   title: __('Lire la suite', 'amnesty'),
   description: 'Block Lire la suite',
   category: 'amnesty-core',
+  icon: 'editor-insertmore',
   attributes: {},
+  example: {
+    attributes: {},
+    innerBlocks: [
+      {
+        name: 'amnesty-core/read-also',
+        attributes: {
+          linkType: 'external',
+          externalUrl: '#',
+          externalLabel: 'Notre rapport complet sur la situation des droits humains en 2025',
+          targetBlank: true,
+        },
+      },
+    ],
+  },
   edit: EditComponent,
   save: SaveComponent,
 });
