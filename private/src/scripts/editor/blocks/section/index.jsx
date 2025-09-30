4e2d253e8d8e1b02a84a87400ae401df92330f90
f-jost <frederic@les-tilleuls.coop>
2025-10-01 01:12:27 +0200

    feat: add section block preview

--- private/src/scripts/editor/blocks/section/index.jsx
+++ private/src/scripts/editor/blocks/section/index.jsx
@@ -8,6 +8,7 @@ registerBlockType('amnesty-core/section', {
   title: __('Section', 'amnesty'),
   description: 'Block section',
   category: 'amnesty-core',
+  icon: 'layout',
   attributes: {
     sectionSize: {
       type: 'string',
@@ -33,6 +34,32 @@ registerBlockType('amnesty-core/section', {
       type: 'string',
       default: 'black',
     },
+  },
+  example: {
+    attributes: {
+      sectionSize: 'large',
+      title: 'Notre Impact en Chiffres',
+      showTitle: true,
+      fullWidth: true,
+      contentSize: 'md',
+      backgroundColor: 'grey',
+    },
+    innerBlocks: [
+      {
+        name: 'core/paragraph',
+        attributes: {
+          content:
+            'Chaque année, nos actions collectives entraînent des changements significatifs à travers le monde. Voici quelques exemples de ce que nous avons accompli ensemble.',
+        },
+      },
+      {
+        name: 'amnesty-core/key-figure',
+        attributes: {
+          title: '1.2 Million',
+          text: "de signatures collectées pour la libération de prisonniers d'opinion.",
+        },
+      },
+    ],
   },
   edit: EditComponent,
   save: SaveComponent,
