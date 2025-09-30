ec61a0c9baccb47040b2af66b8b03a7184bb8015
f-jost <frederic@les-tilleuls.coop>
2025-10-01 01:42:11 +0200

    feat: add video + pop-in blocks previews

--- private/src/scripts/editor/fse-blocks/pop-in/index.jsx
+++ private/src/scripts/editor/fse-blocks/pop-in/index.jsx
@@ -1,8 +1,41 @@
 import DisplayComponent from './DisplayComponent.jsx';
 
 const { registerBlockType } = wp.blocks;
+const { __ } = wp.i18n;
 
 registerBlockType('amnesty-core/pop-in', {
+  title: __('Pop-in', 'amnesty'),
+  category: 'amnesty-core',
+  icon: 'external',
+  attributes: {},
+  example: {
+    innerBlocks: [
+      {
+        name: 'core/heading',
+        attributes: {
+          level: 3,
+          content: 'Rejoignez le mouvement',
+          textAlign: 'center',
+        },
+      },
+      {
+        name: 'core/paragraph',
+        attributes: {
+          content:
+            'Chaque signature compte. Ajoutez la vôtre pour défendre les droits humains partout dans le monde.',
+          align: 'center',
+        },
+      },
+      {
+        name: 'amnesty-core/button',
+        attributes: {
+          text: 'Je signe la pétition',
+          url: '#',
+          align: 'center',
+        },
+      },
+    ],
+  },
   edit: DisplayComponent,
   save: () => null,
 });
