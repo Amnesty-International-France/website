f3893b83c34d4641d00a348ab211e3caf35fc96f
f-jost <frederic@les-tilleuls.coop>
2025-09-30 16:47:46 +0200

    feat: add content-callout block preview

--- private/src/scripts/editor/blocks/content-callout/index.jsx
+++ private/src/scripts/editor/blocks/content-callout/index.jsx
@@ -4,9 +4,8 @@ const { registerBlockType } = wp.blocks;
 const { __ } = wp.i18n;
 
 registerBlockType('amnesty/content-callout', {
-  apiVersion: 2,
   title: __('Mise en exergue', 'amnesty'),
-  category: 'amnesty-blocks',
+  category: 'amnesty-core',
   icon: 'megaphone',
   description: __(
     "Un bloc pour attirer l'attention sur une information clé, avec un titre et un texte.",
@@ -21,6 +20,12 @@ registerBlockType('amnesty/content-callout', {
       type: 'string',
       default:
         'Consectetur adipiscing elit. Curabitur nec neque erat. Vestibulum molestie sem augue, ac congue nulla faucibus id. Sed placerat scelerisque tristique.',
+    },
+  },
+  example: {
+    attributes: {
+      title: 'Notre constat',
+      text: 'Plus de 75% des défenseurs des droits humains interrogés déclarent avoir subi des menaces directes en raison de leur travail.',
     },
   },
   edit: EditComponent,
