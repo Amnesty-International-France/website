5c3b4a92d3bb20b35894d827c73d7ac48cf9c909
f-jost <frederic@les-tilleuls.coop>
2025-09-30 16:46:09 +0200

    feat: add chapo block preview

--- private/src/scripts/editor/blocks/chapo/index.jsx
+++ private/src/scripts/editor/blocks/chapo/index.jsx
@@ -8,10 +8,16 @@ registerBlockType('amnesty-core/chapo', {
   title: __('Chapo', 'amnesty'),
   description: 'Block Chapo',
   category: 'amnesty-core',
+  icon: 'editor-paragraph',
   attributes: {
     text: {
       type: 'string',
       default: '',
+    },
+  },
+  example: {
+    attributes: {
+      text: 'Dans un monde où les libertés fondamentales sont constamment menacées, chaque voix compte. Découvrez comment nos actions contribuent à défendre les droits humains pour toutes et tous.',
     },
   },
   edit: EditComponent,
