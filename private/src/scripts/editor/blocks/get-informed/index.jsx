eb2d4a3940eba84137babd98b7939699addcc2ea
f-jost <frederic@les-tilleuls.coop>
2025-09-30 16:58:37 +0200

    feat: add get-informed block preview

--- private/src/scripts/editor/blocks/get-informed/index.jsx
+++ private/src/scripts/editor/blocks/get-informed/index.jsx
@@ -7,10 +7,35 @@ registerBlockType('amnesty-core/get-informed', {
   title: __("S'informer", 'amnesty'),
   description: "Block s'informer",
   category: 'amnesty-core',
+  icon: 'info-outline',
   attributes: {
     links: {
       type: 'array',
       default: [],
+    },
+  },
+  example: {
+    attributes: {
+      links: [
+        {
+          type: 'dossier',
+          title: 'Comprendre la crise des droits humains au Darfour',
+          url: '#',
+          customLabel: '',
+        },
+        {
+          type: 'pays',
+          title: 'Situation actuelle en Iran : un combat pour la liberté',
+          url: '#',
+          customLabel: '',
+        },
+        {
+          type: 'libre',
+          title: 'Notre rapport annuel est disponible en téléchargement',
+          url: '#',
+          customLabel: 'TÉLÉCHARGER LE RAPPORT',
+        },
+      ],
     },
   },
   edit: EditComponent,
