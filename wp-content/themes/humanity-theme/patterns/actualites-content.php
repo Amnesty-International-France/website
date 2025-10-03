44e923d165a51694f22a9504d9285577dc2803ad
Mehdi Zaidi <mehdi@les-tilleuls.coop>
2025-10-03 16:08:48 +0200

    fix: remove filter on my space actualities pages

--- wp-content/themes/humanity-theme/patterns/actualites-content.php
+++ wp-content/themes/humanity-theme/patterns/actualites-content.php
@@ -12,7 +12,6 @@
     <!-- wp:pattern {"slug":"amnesty/my-space-header"} /-->
     <div class="aif-actualities">
         <h2 class="aif-actualities-title"><?php the_title(); ?></h2>
-        <!-- wp:amnesty-core/archive-filters-actualities /-->
         <!-- wp:pattern {"slug":"amnesty/actualites-loop"} /-->
     </div>
 </main>
