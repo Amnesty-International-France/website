6630b7c34f5cefce73ac41b9791f126293f71fea
Mehdi Zaidi <mehdi@les-tilleuls.coop>
2025-10-06 16:13:08 +0200

    fix: check if urgent banner is null

--- wp-content/themes/humanity-theme/partials/urgent-banner.php
+++ wp-content/themes/humanity-theme/partials/urgent-banner.php
@@ -9,82 +9,77 @@ $args = [
     'order'          => 'DESC',
 ];
 
-$last_post = new WP_Query($args);
+$query = new WP_Query($args);
+$popin = $query->post;
+if (!$popin) {
+    return;
+}
 
-$svg               = file_get_contents(get_template_directory() . '/assets/images/icon-cross.svg');
-$logo              = file_get_contents(get_template_directory() . '/assets/images/icon-logo-fr.svg');
-$thumbnail         = get_the_post_thumbnail($last_post->post);
-$link_action_popin = get_field('link_action_popin', $last_post->post->ID);
-$button_text       = get_field('button_text_popin', $last_post->post->ID);
-$button_icon       = file_get_contents(get_template_directory() . '/assets/images/icon-arrow.svg');
+$thumbnail         = get_the_post_thumbnail($popin);
+$link_action_popin = get_field('link_action_popin', $popin->ID);
+$button_text       = get_field('button_text_popin', $popin->ID);
 
-if ($last_post->have_posts()) :
-    while ($last_post->have_posts()) :
-        $last_post->the_post(); ?>
-
-		<div class="urgent-banner hidden">
-			<div class="urgent-banner-modal
-			<?php
-            if ($thumbnail) {
-                echo 'with-image';
-            }
-        ?>
-			">
-				<div class="urgent-container">
-					<?php if ($thumbnail) : ?>
-						<div class="left">
-							<?php echo $thumbnail; ?>
-						</div>
-					<?php endif; ?>
-
-					<div class="right">
-						<div class="header-icon">
-							<?php echo $logo; ?>
-
-							<?php
-                        echo wp_kses(
-                            $svg,
-                            [
-                                'svg'  => [
-                                    'class'   => true,
-                                    'width'   => true,
-                                    'height'  => true,
-                                    'viewBox' => true,
-                                    'fill'    => true,
-                                    'xmlns'   => true,
-                                ],
-                                'path' => [
-                                    'd'               => true,
-                                    'stroke'          => true,
-                                    'stroke-width'    => true,
-                                    'stroke-linecap'  => true,
-                                    'stroke-linejoin' => true,
-                                ],
-                            ]
-                        );
-        ?>
-						</div>
-						<div class="body">
-                            <div class="content-wrapper">
-                                <h1 class="title"> <?php echo esc_html(get_the_title($last_post->post->ID)); ?></h1>
-                                <p class="content"><?php the_field('popin_content'); ?></p>
-                            </div>
-							<a class="cta"
-								href="<?php echo esc_url($link_action_popin); ?>"
-								target="_blank"
-							>
-								<?php echo $button_icon; ?>
-								<?php echo esc_html($button_text); ?>
-							</a>
-						</div>
-                        
+$template_directory = get_template_directory();
+$svg               = file_get_contents($template_directory . '/assets/images/icon-cross.svg');
+$logo              = file_get_contents($template_directory . '/assets/images/icon-logo-fr.svg');
+$button_icon       = file_get_contents($template_directory . '/assets/images/icon-arrow.svg');
+?>
+<div class="urgent-banner hidden">
+	<div class="urgent-banner-modal <?= $thumbnail ? 'with-image' : '' ?>">
+		<div class="urgent-container">
+			<?php if ($thumbnail) : ?>
+				<div class="left">
+					<?php echo $thumbnail; ?>
+				</div>
+			<?php endif; ?>
+			<div class="right">
+				<div class="header-icon">
+					<?php echo $logo; ?>
+					<?php
+                    echo wp_kses(
+                        $svg,
+                        [
+                            'svg'  => [
+                                'class'   => true,
+                                'width'   => true,
+                                'height'  => true,
+                                'viewBox' => true,
+                                'fill'    => true,
+                                'xmlns'   => true,
+                            ],
+                            'path' => [
+                                'd'               => true,
+                                'stroke'          => true,
+                                'stroke-width'    => true,
+                                'stroke-linecap'  => true,
+                                'stroke-linejoin' => true,
+                            ],
+                        ]
+                    );
+?>
+				</div>
+				<div class="body">
+					<div class="content-wrapper">
+						<h1 class="title">
+							<?php echo esc_html(get_the_title($query->post->ID)); ?>
+						</h1>
+						<p class="content">
+							<?php the_field('popin_content'); ?>
+						</p>
 					</div>
+					<a class="cta"
+					   href="
+						<?php echo esc_url($link_action_popin); ?>"
+					   target="_blank"
+					>
+						<?php echo $button_icon; ?>
+						<?php echo esc_html($button_text); ?>
+					</a>
 				</div>
 			</div>
 		</div>
-		<?php
-    endwhile;
+	</div>
+</div>
+<?php
     wp_reset_postdata();
-endif;
-
 ?>
