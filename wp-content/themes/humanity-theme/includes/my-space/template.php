1ae5d971aa4cbe527a4c6e7bb015e3cb385df9e3
Antoine Cau <antoine.cau@les-tilleuls.coop>
2025-10-16 11:47:55 +0200

    allowed pages for non members

--- wp-content/themes/humanity-theme/includes/my-space/template.php
+++ wp-content/themes/humanity-theme/includes/my-space/template.php
@@ -4,7 +4,7 @@ function include_default_template_for_my_space($template)
 {
     global $post;
 
-    if (! is_page() || !$post) {
+    if (! is_page() || ! $post) {
         return $template;
     }
 
@@ -29,27 +29,70 @@ function include_default_template_for_my_space($template)
     }
     return $template;
 }
-add_action('template_include', 'include_default_template_for_my_space');
 
-add_action('template_redirect', 'auth_my_space');
 
-function auth_my_space()
+function my_space_access_control()
 {
-    $slug_parent_page = 'mon-espace';
+    if (is_admin() || is_preview()) {
+        return;
+    }
 
-    if (is_page() && ! is_preview()) {
+    $my_space_slug = 'mon-espace';
+
+    $is_my_space_page = false;
+    if (is_page()) {
         $current_page = get_queried_object();
-
-        $parent_page = get_page_by_path($slug_parent_page);
+        $parent_page = get_page_by_path($my_space_slug);
 
         if ($parent_page) {
-            $id_parent_page = $parent_page->ID;
-
-            $ancestors = get_post_ancestors($current_page->ID);
-
-            if ($current_page->ID === $id_parent_page || in_array($id_parent_page, $ancestors)) {
-                check_user_page_access();
-            }
+            $is_my_space_page = ($current_page->ID === $parent_page->ID || in_array($parent_page->ID, get_post_ancestors($current_page->ID)));
         }
     }
+
+    if (! $is_my_space_page) {
+        return;
+    }
+
+    check_user_page_access();
+
+    if (!is_user_logged_in()) {
+        return;
+    }
+
+    $is_member = false;
+    if (function_exists('get_salesforce_member_data')) {
+        $current_user = wp_get_current_user();
+        $sf_member = get_salesforce_member_data($current_user->user_email);
+        if (isset($sf_member) && !empty($sf_member->isMembre)) {
+            $is_member = true;
+        }
+    }
+
+    if ($is_member) {
+        return;
+    }
+
+    $allowed_for_non_members = [
+        'mes-dons',
+        'mes-informations-personnelles',
+        'mes-recus-fiscaux',
+        'mes-demandes',
+        'mon-compte',
+    ];
+
+    $non_member_homepage = home_url('/' . $my_space_slug . '/' . $allowed_for_non_members[0] . '/');
+    $current_page_slug = get_post()->post_name;
+
+    if (is_page($my_space_slug)) {
+        wp_redirect($non_member_homepage);
+        exit;
+    }
+
+    if (!in_array($current_page_slug, $allowed_for_non_members)) {
+        wp_redirect($non_member_homepage);
+        exit;
+    }
 }
+
+add_action('template_include', 'include_default_template_for_my_space');
+add_action('template_redirect', 'my_space_access_control');
