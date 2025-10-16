aa1f650cbfab714eae0288b6c237ecb1c3ecf926
Valentin Dassonville <valentin.dassonville59@gmail.com>
2025-10-16 14:29:11 +0200

    fix: amn-354 open rapport in new tab

---
+++ wp-content/themes/humanity-theme/includes/blocks/document-card/render.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+if (!function_exists('render_document_card_block')) {
+
+    /**
+     * Render Document Card Block
+     *
+     * @param array<string,mixed> $attributes the block attributes
+     *
+     * @return string
+     * @package Amnesty\Blocks
+     *
+     */
+    function render_document_card_block($attributes, $content, $block): string
+    {
+        $direction = $attributes['direction'] ?? 'portrait';
+
+        if (!empty($attributes['postId']) && !is_admin()) {
+            $post = get_post((int) $attributes['postId']);
+            if ($post) {
+                setup_postdata($post);
+            }
+        } elseif (empty($attributes['postId']) && isset($GLOBALS['post'])) {
+            $post = $GLOBALS['post'];
+        }
+
+        if (isset($post) && $post) {
+            $post_id = $post->ID;
+            $attachment = get_field('upload_du_document', $post_id);
+            $permalink = wp_get_attachment_url($attachment['ID']);
+            $title = get_the_title($post);
+            $date = get_the_date('', $post);
+            $thumbnail = get_the_post_thumbnail($post->ID, 'medium', ['class' => 'article-image']);
+            $post_terms = wp_get_object_terms($post->ID, get_object_taxonomies(get_post_type($post)));
+        }
+
+        if (!empty($attributes['postId']) && ! is_admin()) {
+            wp_reset_postdata();
+        }
+
+        $args = [
+            'direction' => $direction,
+            'title' => $title,
+            'permalink' => $permalink,
+            'date' => $date,
+            'thumbnail' => $thumbnail,
+            'terms' => $post_terms,
+            'label' => 'Rapport',
+        ];
+
+        if (isset($post_id)) {
+            $args['post_id'] = $post_id;
+        }
+
+        ob_start();
+        $template_path = locate_template('partials/document-card.php');
+        if ($template_path) {
+            extract($args);
+            include $template_path;
+        }
+        return ob_get_clean();
+    }
+}
---
+++ wp-content/themes/humanity-theme/partials/document-card.php
@@ -0,0 +1,56 @@
+<article class="article-card card-<?php echo esc_attr($direction); ?>">
+    <?php if ($thumbnail) : ?>
+        <a href="<?= esc_url($permalink); ?>" class="article-thumbnail" target="_blank" rel="noopener noreferrer">
+            <?= $thumbnail; ?>
+        </a>
+    <?php else : ?>
+        <div class="article-thumbnail"></div>
+    <?php endif; ?>
+
+    <?php if (!empty($label)): ?>
+        <?= render_chip_category_block([
+            'label' => esc_html($label),
+            'link' => esc_url($link),
+            'size' => 'large',
+            'style' => 'bg-yellow',
+            'icon' => $icon ?? '',
+        ]); ?>
+    <?php endif; ?>
+
+    <div class="article-content">
+        <time class="article-date" datetime="<?= esc_attr(date('c', strtotime($date))); ?>">
+            <?= esc_html($date); ?>
+        </time>
+        <div class="article-title">
+            <a class="as-h5" href="<?= esc_url($permalink); ?>" target="_blank" rel="noopener noreferrer" >
+                <?= esc_html($title); ?>
+            </a>
+        </div>
+        <div class="article-terms <?php if (empty($post_terms)) {
+            echo 'is-empty';
+        } ?>">
+            <?php foreach ($post_terms as $term): ?>
+                <?php
+                $term_link = get_term_link($term);
+
+                if (!is_wp_error($term_link)) {
+                    $custom_routes = [
+                        'combat'   => '/combats/',
+                        'location' => '/categorie/',
+                    ];
+
+                    if (array_key_exists($term->taxonomy, $custom_routes)) {
+                        $term_link = $custom_routes[$term->taxonomy] . $term->slug;
+                    }
+                }
+                ?>
+                <?= render_chip_category_block([
+                    'label' => esc_html($term->name),
+                    'size'  => 'small',
+                    'style' => 'bg-gray',
+                    'link'  => esc_url($term_link),
+                ]) ?>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</article>
