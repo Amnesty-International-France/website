45914f13085a4f21dd83524ddde408e75bdfd7a1
Antoine Cau <antoine.cau@les-tilleuls.coop>
2025-10-20 16:33:08 +0200

    call to action block icon button

--- wp-content/themes/humanity-theme/includes/blocks/call-to-action/render.php
+++ wp-content/themes/humanity-theme/includes/blocks/call-to-action/render.php
@@ -42,15 +42,7 @@ if (!function_exists('render_call_to_action_block')) {
                 <a href="<?php echo esc_url($final_link); ?>"<?php echo $target_attr; ?> class="custom-button">
                     <div class='content bg-yellow medium'>
                         <div class="icon-container">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                            >
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
-                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" /></svg>
                         </div>
                         <div class="button-label"><?php echo esc_html($buttonLabel); ?></div>
                     </div>
