ec598c3f705d9fa409c5d66debfa720e0d9bf25c
Antoine Cau <antoine.cau@les-tilleuls.coop>
2025-10-24 11:53:48 +0200

    location archive pagination linter

--- wp-content/themes/humanity-theme/functions.php
+++ wp-content/themes/humanity-theme/functions.php
@@ -6,12 +6,12 @@
  * @package Amnesty
  */
 
-add_filter( 'redirect_canonical', function( $redirect_url, $requested_url ) {
-    if ( is_tax('location') && is_paged() ) {
+add_filter('redirect_canonical', function ($redirect_url, $requested_url) {
+    if (is_tax('location') && is_paged()) {
         return false;
     }
     return $redirect_url;
-}, 10, 2 );
+}, 10, 2);
 
 /**
  * Theme root includes
