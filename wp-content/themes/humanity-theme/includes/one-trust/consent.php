a1fc39c37725ac3817450bc705fff78949a35ee4
Antoine Cau <antoine.cau@les-tilleuls.coop>
2025-10-27 14:00:40 +0100

    consent on trust v3

--- wp-content/themes/humanity-theme/includes/one-trust/consent.php
+++ wp-content/themes/humanity-theme/includes/one-trust/consent.php
@@ -5,10 +5,10 @@
 function amnesty_add_onetrust_script()
 {
     ?>
-    <script src="https://cdn.cookielaw.org/consent/aa4c3202-60d0-4563-8ecc-3a0be4f2b0b6-test/otSDKStub.js"  
+    <script src="https://cdn.cookielaw.org/consent/019a2567-55d4-7c00-907c-30ce2f0c9eed-test/otSDKStub.js"  
             type="text/javascript" 
             charset="UTF-8" 
-            data-domain-script="aa4c3202-60d0-4563-8ecc-3a0be4f2b0b6-test" ></script>
+            data-domain-script="019a2567-55d4-7c00-907c-30ce2f0c9eed-test" ></script>
     <script type="text/javascript">
       function OptanonWrapper() { }
     </script>
