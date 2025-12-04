0797a3952fa5ac58d055f84305e621dc44cb9699
Valentin Dassonville <valentin.dassonville59@gmail.com>
2025-12-04 15:36:51 +0100

    fix: amn-668

--- wp-content/themes/humanity-theme/tribe/events/single-event-blocks.php
+++ wp-content/themes/humanity-theme/tribe/events/single-event-blocks.php
@@ -181,14 +181,14 @@ if ($post_type_object->name === 'tribe_events') {
 		<div class="event-near">
 			<h3>Près de chez vous</h3>
 			<h5>Trouvez d’autres événements pour agir avec nous</h5>
-			<button id="localisation">
+			<a href="<?= get_post_type_archive_link($post_type) ?>">
 				<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path fill-rule="evenodd" clip-rule="evenodd"
 						  d="M16.172 11L10.808 5.636L12.222 4.222L20 12L12.222 19.778L10.808 18.364L16.172 13H4V11H16.172Z"
 						  fill="black"/>
 				</svg>
 				rechercher
-			</button>
+			</a>
 		</div>
 
 		<!-- wp:group {"tagName":"footer","className":"article-footer"} -->
