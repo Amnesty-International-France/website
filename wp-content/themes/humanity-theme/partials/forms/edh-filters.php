6a8261fb51e8940d7bbaba91fa8aac5190e26c9c
Valentin Dassonville <valentin.dassonville59@gmail.com>
2025-12-11 17:34:13 +0100

    fix: amn-690

--- wp-content/themes/humanity-theme/partials/forms/edh-filters.php
+++ wp-content/themes/humanity-theme/partials/forms/edh-filters.php
@@ -10,28 +10,28 @@ declare(strict_types=1);
 
 global $wpdb;
 
-$active_content_type  = isset($_GET['qtype_de_contenu']) ? explode(',', sanitize_text_field($_GET['qtype_de_contenu'])) : [];
+$active_content_type  = isset($_GET['qtype_de_contenu']) ? sanitize_text_field($_GET['qtype_de_contenu']) : null;
 $content_type_options = [ '' => 'Tous les types de contenus' ];
 $cat_field_obj        = get_field_object('field_68c2a14b84414');
 if ($cat_field_obj && isset($cat_field_obj['choices'])) {
     $content_type_options += $cat_field_obj['choices'];
 }
 
-$active_theme    = isset($_GET['qtheme']) ? explode(',', sanitize_text_field($_GET['qtheme'])) : [];
+$active_theme    = isset($_GET['qtheme']) ? sanitize_text_field($_GET['qtheme']) : null;
 $theme_options   = [ '' => 'Tous les theme' ];
 $theme_field_obj = get_field_object('field_68c2a1a484415');
 if ($theme_field_obj && isset($theme_field_obj['choices'])) {
     $theme_options += $theme_field_obj['choices'];
 }
 
-$active_requirements    = isset($_GET['qrequirements']) ? explode(',', sanitize_text_field($_GET['qrequirements'])) : [];
+$active_requirements    = isset($_GET['qrequirements']) ? sanitize_text_field($_GET['qrequirements']) : null;
 $requirements_options   = [ '' => 'Tous les Besoins' ];
 $requirements_field_obj = get_field_object('field_68c2a1f984416');
 if ($requirements_field_obj && isset($requirements_field_obj['choices'])) {
     $requirements_options += $requirements_field_obj['choices'];
 }
 
-$active_activity_duration    = isset($_GET['qactivity_duration']) ? explode(',', sanitize_text_field($_GET['qactivity_duration'])) : [];
+$active_activity_duration    = isset($_GET['qactivity_duration']) ? sanitize_text_field($_GET['qactivity_duration']) : null;
 $activity_duration_options   = [ '' => 'Tous les Besoins' ];
 $activity_duration_field_obj = get_field_object('field_68c2a22184417');
 if ($activity_duration_field_obj && isset($activity_duration_field_obj['choices'])) {
@@ -45,39 +45,39 @@ if ($activity_duration_field_obj && isset($activity_duration_field_obj['choices'
     amnesty_render_custom_select(
         [
             'label'    => 'Type de contenu',
+            'show_label' => true,
             'name'     => 'qtype_de_contenu',
             'active'   => $active_content_type,
             'options'  => $content_type_options,
-            'multiple' => true,
         ]
     );
 amnesty_render_custom_select(
     [
         'label'    => 'Thème',
+        'show_label' => true,
         'name'     => 'qtheme',
         'active'   => $active_theme,
         'options'  => $theme_options,
-        'multiple' => true,
     ]
 );
 amnesty_render_custom_select(
     [
         'label'    => 'Besoins',
+        'show_label' => true,
         'name'     => 'qrequirements',
         'active'   => $active_requirements,
         'options'  => $requirements_options,
-        'multiple' => true,
     ]
 );
 amnesty_render_custom_select(
     [
         'label'    => 'Durée de l\'activité',
+        'show_label' => true,
         'name'     => 'qactivity_duration',
         'active'   => $active_activity_duration,
         'options'  => $activity_duration_options,
-        'multiple' => true,
     ]
 );
 ?>
 </div>
-<button id="training-filters-submit" class="filter-button" type="submit">Filtrer</button>
+<a type="button" id="training-filters-submit" class="filter-button" href="<?php echo esc_url($form_url)?>">Réinitialiser</a>
