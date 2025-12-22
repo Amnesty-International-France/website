f1d78b30ee9af0c5ad27f7678067ab4a3eb76cae
Valentin Dassonville <valentin.dassonville59@gmail.com>
2025-12-22 11:36:30 +0100

    stripslashes

--- wp-content/themes/humanity-theme/includes/blocks/get-informed/render.php
+++ wp-content/themes/humanity-theme/includes/blocks/get-informed/render.php
@@ -73,7 +73,7 @@ function render_get_informed_block(array $attributes): string
                                     href="<?php echo esc_url($url); ?>"
                                     <?php echo ($type === 'video') ? 'target="_blank" rel="noopener noreferrer"' : ''; ?>
                                 >
-                                    <?php echo wp_kses_post($title); ?>
+                                    <?php echo wp_kses_post(stripslashes($title)); ?>
                                 </a>
                             <?php endif; ?>
                         </div>
