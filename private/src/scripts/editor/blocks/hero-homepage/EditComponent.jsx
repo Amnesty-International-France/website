3eb00658e28716cf56dbbcd59a7d07f6e5f47469
Valentin Dassonville <valentin.dassonville59@gmail.com>
2025-12-23 11:27:03 +0100

    feat: amn-687

--- private/src/scripts/editor/blocks/hero-homepage/EditComponent.jsx
+++ private/src/scripts/editor/blocks/hero-homepage/EditComponent.jsx
@@ -3,7 +3,7 @@ import CustomButton from '../button/Button.jsx';
 const { __ } = wp.i18n;
 const { useBlockProps, InspectorControls, MediaUpload, MediaUploadCheck, URLInput } =
   wp.blockEditor;
-const { PanelBody, Button, TextControl } = wp.components;
+const { PanelBody, BaseControl, Button, TextControl } = wp.components;
 const { useSelect } = wp.data;
 
 const useMedias = (mediaIds) =>
@@ -25,39 +25,73 @@ const EditComponent = ({ attributes, setAttributes }) => {
   const mediaIds = items.map((item) => item.mediaId);
   const mediaList = useMedias(mediaIds);
 
+  const mediaMobileIds = items.map((item) => item.mediaMobileId);
+  const mediaMobileList = useMedias(mediaMobileIds);
+
   const visibleItem = items[0];
   const visibleMedia = mediaList[0];
+  const visibleMobileMedia = mediaMobileList[0];
 
   return (
     <>
       <InspectorControls>
         {items.map((item, index) => {
           const itemMedia = mediaList[index];
+          const itemMobileMedia = mediaMobileList[index];
 
           return (
             <PanelBody key={index} title={`Bloc ${index + 1}`} initialOpen={index === 0}>
-              <MediaUploadCheck>
-                <MediaUpload
-                  onSelect={(selectedMedia) => updateItem(index, 'mediaId', selectedMedia.id)}
-                  allowedTypes={['image']}
-                  value={item.mediaId}
-                  render={({ open }) => (
-                    <Button onClick={open} isSecondary>
-                      {item.mediaId
-                        ? __('Changer l’image', 'amnesty')
-                        : __('Ajouter une image', 'amnesty')}
-                    </Button>
-                  )}
-                />
-              </MediaUploadCheck>
+              <BaseControl label="Image deskstop">
+                <MediaUploadCheck>
+                  <MediaUpload
+                    onSelect={(selectedMedia) => updateItem(index, 'mediaId', selectedMedia.id)}
+                    allowedTypes={['image']}
+                    value={item.mediaId}
+                    render={({ open }) => (
+                      <Button onClick={open} isSecondary>
+                        {item.mediaId
+                          ? __('Changer l’image', 'amnesty')
+                          : __('Ajouter une image', 'amnesty')}
+                      </Button>
+                    )}
+                  />
+                </MediaUploadCheck>
 
-              {itemMedia?.source_url && (
-                <img
-                  src={itemMedia.source_url}
-                  alt=""
-                  style={{ maxWidth: '100%', height: 'auto', marginTop: '8px' }}
-                />
-              )}
+                {itemMedia?.source_url && (
+                  <img
+                    src={itemMedia.source_url}
+                    alt=""
+                    style={{ maxWidth: '100%', height: 'auto', marginTop: '8px' }}
+                  />
+                )}
+              </BaseControl>
+
+              <BaseControl label="Image mobile">
+                <MediaUploadCheck>
+                  <MediaUpload
+                    onSelect={(selectedMedia) =>
+                      updateItem(index, 'mediaMobileId', selectedMedia.id)
+                    }
+                    allowedTypes={['image']}
+                    value={item.mediaMobileId}
+                    render={({ open }) => (
+                      <Button onClick={open} isSecondary>
+                        {item.mediaMobileId
+                          ? __('Changer l’image', 'amnesty')
+                          : __('Ajouter une image', 'amnesty')}
+                      </Button>
+                    )}
+                  />
+                </MediaUploadCheck>
+
+                {itemMobileMedia?.source_url && (
+                  <img
+                    src={itemMobileMedia.source_url}
+                    alt=""
+                    style={{ maxWidth: '100%', height: 'auto', marginTop: '8px' }}
+                  />
+                )}
+              </BaseControl>
 
               <TextControl
                 label={__('Sous titre', 'amnesty')}
@@ -87,6 +121,9 @@ const EditComponent = ({ attributes, setAttributes }) => {
             <div className="hero-wrapper">
               <div className="hero-image-wrapper">
                 <img className="hero-image" src={visibleMedia?.source_url || ''} alt="" />
+              </div>
+              <div className="hero-image-mobile-wrapper">
+                <img className="hero-image" src={visibleMobileMedia?.source_url || ''} alt="" />
               </div>
               <div className="hero-content-wrapper">
                 <h1 className="hero-title">
