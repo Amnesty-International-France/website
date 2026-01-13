6e0a4429f23b917efe8df1786e4db345a07968cf
Antoine Cau <antoine.cau@les-tilleuls.coop>
2026-01-13 16:33:52 +0100

    738: description on carousel images

--- wp-content/themes/humanity-theme/includes/blocks/carousel/render.php
+++ wp-content/themes/humanity-theme/includes/blocks/carousel/render.php
@@ -31,12 +31,20 @@ if (!function_exists('render_carousel_block')) {
                         $image_url = wp_get_attachment_image_url($image_id, 'large');
                         $alt = get_post_meta($image_id, '_wp_attachment_image_alt', true);
                         $caption = wp_get_attachment_caption($image_id);
+                        $description = get_post_field('post_content', $image_id);
                         ?>
                         <div class="swiper-slide">
-                            <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($alt); ?>" loading="lazy" />
-                            <?php if (!empty($caption)) : ?>
-                                <div class="carousel-caption">
-                                    <span class="carousel-caption-text"><?php echo wp_kses_post($caption); ?></span>
+                            <div class="carousel-image">
+                                <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($alt); ?>" loading="lazy" />
+                                <?php if (!empty($caption)) : ?>
+                                    <div class="carousel-caption">
+                                        <span class="carousel-caption-text"><?php echo wp_kses_post($caption); ?></span>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                            <?php if (!empty($description)) : ?>
+                                <div class="carousel-description">
+                                    <span class="carousel-description-text"><?php echo wp_kses_post($description); ?></span>
                                 </div>
                             <?php endif; ?>
                         </div>
