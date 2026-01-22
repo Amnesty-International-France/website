1388ad78b9414813be4fec5f19c01f7c7de65fb9
Valentin Dassonville <valentin.dassonville59@gmail.com>
2026-01-22 17:16:54 +0100

    taggage forms

--- wp-content/themes/humanity-theme/patterns/page-nl-content.php
+++ wp-content/themes/humanity-theme/patterns/page-nl-content.php
@@ -104,6 +104,8 @@ if (!is_admin() && (!defined('REST_REQUEST') || !REST_REQUEST)) {
         wp_redirect(add_query_arg([
             'email' => urlencode($email),
             'inscription__nl' => 'success',
+            'gtm_type' => 'inscription',
+            'gtm_name' => 'newsletter',
         ], home_url('/newsletter')));
         exit;
     }
