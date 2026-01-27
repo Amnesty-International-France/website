9790409500a25c1bf62be516ab838364d7ee58a8
Valentin Dassonville <129871973+valentin-dassonville@users.noreply.github.com>
2026-01-27 13:53:10 +0100

    Revert "fix: search"
    This reverts commit 636eb398fb18d7395c0aecb1310c21fd05ac41d9.

--- wp-content/themes/humanity-theme/patterns/militants-resources-loop.php
+++ wp-content/themes/humanity-theme/patterns/militants-resources-loop.php
@@ -19,7 +19,7 @@ $meta_query = [
     ],
 ];
 
-$search = isset($_GET['q']) ? $_GET['q'] : null;
+$search = isset($_GET['q']) ? sanitize_text_field($_GET['q']) : null;
 $posts_per_page = $search ? -1 : 30;
 
 $args = [
@@ -50,10 +50,6 @@ if ($type_filter) {
 $args['tax_query'] = $tax_query;
 
 if ($search) {
-    $search = str_replace(['«', '»', '"', '“', '”', ' '], ' ', $search);
-    $search = preg_replace('/\s+/u', ' ', $search);
-    $search = sanitize_text_field($search);
-    $search = trim($search);
     $args['s'] = $search;
     unset($args['paged']);
 }
