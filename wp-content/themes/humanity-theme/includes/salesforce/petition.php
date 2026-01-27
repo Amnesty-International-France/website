8a1551d36caadedef26a40aea7f099769f3aa509
Valentin Dassonville <129871973+valentin-dassonville@users.noreply.github.com>
2026-01-27 13:53:10 +0100

    Revert "fix: error type patch sf"
    This reverts commit 7d27ecf430a6a4b9315d56b1a8f36ae1c7adc1ae.

--- wp-content/themes/humanity-theme/includes/salesforce/petition.php
+++ wp-content/themes/humanity-theme/includes/salesforce/petition.php
@@ -16,7 +16,7 @@ function post_salesforce_petition(array $data): string|false
     return false;
 }
 
-function patch_salesforce_petition(string $external_id, array $data)
+function patch_salesforce_petition(string $external_id, array $data): bool
 {
     $url = "services/data/v57.0/sobjects/Petition__c/Ext_ID_Petition__c/$external_id";
     return patch_salesforce_data($url, $data);
