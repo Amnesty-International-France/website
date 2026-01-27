a85d08ff96d635f4513a522eabdbc60d2fd407bd
Valentin Dassonville <129871973+valentin-dassonville@users.noreply.github.com>
2026-01-27 13:53:10 +0100

    Revert "fix: amn-747"
    This reverts commit 024c274da03d5d0ecd2587536ff76d6bd5f28ba0.

--- wp-content/themes/humanity-theme/includes/document/rest_endpoint.php
+++ wp-content/themes/humanity-theme/includes/document/rest_endpoint.php
@@ -1,49 +1,29 @@
 <?php
 
-function title_filter($where, $wp_query)
-{
-    global $wpdb;
-    if ($search_term = $wp_query->get('search_title')) {
-        $where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . $wpdb->esc_like($search_term) . '%\'';
-    }
-    return $where;
-}
-
-function search_resources_by_taxonomies(WP_REST_Request $request, array $taxonomies)
+function search_resources_by_taxonomy(WP_REST_Request $request, string $taxonomy)
 {
     $term = $request->get_param('term');
 
-    $tax_query = [];
-    foreach ($taxonomies as $taxonomy) {
-        $tax_query[] = [
-            'taxonomy' => $taxonomy,
-            'operator' => 'EXISTS',
-        ];
-    }
-
     $args = [
-        'post_type'      => 'document',
-        'post_status'    => 'publish',
+        'post_type' => 'document',
         'posts_per_page' => 5,
-        'tax_query'      => $tax_query,
-        'meta_query'     => [
+        'tax_query' => [
             [
-                'key'     => 'document_private',
-                'value'   => true,
-                'compare' => '=',
+                'taxonomy' => $taxonomy,
+                'operator' => 'EXISTS',
             ],
         ],
-        'search_title'   => $term,
+        's' => $term,
     ];
 
-    $query   = new WP_Query($args);
+    $query = new WP_Query($args);
     $results = [];
     if ($query->have_posts()) {
         while ($query->have_posts()) {
             $query->the_post();
             $results[] = [
                 'title' => str_replace('&#8211;', '-', get_the_title()),
-                'link'  => get_permalink(),
+                'link' => get_permalink(),
             ];
         }
     }
@@ -53,47 +33,36 @@ function search_resources_by_taxonomies(WP_REST_Request $request, array $taxonom
 
 function rest_search_document_militant(WP_REST_Request $request)
 {
-    return rest_ensure_response(search_resources_by_taxonomies($request, ['document_militant_type']));
+    return rest_ensure_response(search_resources_by_taxonomy($request, 'document_militant_type'));
 }
 
 function rest_search_document_democratic(WP_REST_Request $request)
 {
-    return rest_ensure_response(search_resources_by_taxonomies($request, ['document_democratic_type']));
+    return rest_ensure_response(search_resources_by_taxonomy($request, 'document_democratic_type'));
 }
 
-add_action(
-    'rest_api_init',
-    function () {
-        register_rest_route(
-            'humanity/v1',
-            '/search-document-militant',
-            [
-                'methods'             => 'GET',
-                'callback'            => 'rest_search_document_militant',
-                'permission_callback' => '__return_true',
-                'args'                => [
-                    'term' => [
-                        'required'          => true,
-                        'sanitize_callback' => 'sanitize_text_field',
-                    ],
-                ],
-            ]
-        );
+add_action('rest_api_init', function () {
+    register_rest_route('humanity/v1', '/search-document-militant', [
+        'methods' => 'GET',
+        'callback' => 'rest_search_document_militant',
+        'permission_callback' => '__return_true',
+        'args' => [
+            'term' => [
+                'required' => true,
+                'sanitize_callback' => 'sanitize_text_field',
+            ],
+        ],
+    ]);
 
-        register_rest_route(
-            'humanity/v1',
-            '/search-document-democratic',
-            [
-                'methods'             => 'GET',
-                'callback'            => 'rest_search_document_democratic',
-                'permission_callback' => '__return_true',
-                'args'                => [
-                    'term' => [
-                        'required'          => true,
-                        'sanitize_callback' => 'sanitize_text_field',
-                    ],
-                ],
-            ]
-        );
-    }
-);
+    register_rest_route('humanity/v1', '/search-document-democratic', [
+        'methods' => 'GET',
+        'callback' => 'rest_search_document_democratic',
+        'permission_callback' => '__return_true',
+        'args' => [
+            'term' => [
+                'required' => true,
+                'sanitize_callback' => 'sanitize_text_field',
+            ],
+        ],
+    ]);
+});
