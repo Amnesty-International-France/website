dace1e8001b0ba538feca91f91a01d5adca7e771
Antoine Cau <antoine.cau@les-tilleuls.coop>
2026-01-30 11:34:58 +0100

    fix amn-812: newsletter redirection

--- wp-content/themes/humanity-theme/functions.php
+++ wp-content/themes/humanity-theme/functions.php
@@ -295,6 +295,7 @@ require_once realpath(__DIR__ . '/includes/salesforce/petition.php');
 require_once realpath(__DIR__ . '/includes/salesforce/user.php');
 require_once realpath(__DIR__ . '/includes/salesforce/case.php');
 require_once realpath(__DIR__ . '/includes/salesforce/newsletter.php');
+require_once realpath(__DIR__ . '/includes/newsletter/footer-lead.php');
 
 /**
  * Document module
