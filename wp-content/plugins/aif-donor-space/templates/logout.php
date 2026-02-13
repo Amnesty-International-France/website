d20cbc54a19b4e8718d9823e264d1b441959b2eb
Antoine Cau <antoine.cau@les-tilleuls.coop>
2026-02-13 14:53:41 +0100

    remove unused files

--- wp-content/plugins/aif-donor-space/templates/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-wp_logout();
-
-wp_redirect(get_permalink(get_page_by_path('connectez-vous')));
-exit;
--- wp-content/plugins/aif-donor-space/templates/welcome.php
+++
@@ -1,4 +0,0 @@
-<div class="aif-welcome-card" style="padding: 20px; text-align: center;">
-	<h2>Bienvenue sur votre espace</h2>
-	<p>Utilisez le menu sur la gauche pour naviguer.</p>
-</div>
