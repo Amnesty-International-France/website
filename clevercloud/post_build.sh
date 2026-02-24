a6070de9e53795ccc4ffaec42e808df3f7680c16
Mehdi Zaidi <mehdi@les-tilleuls.coop>
2026-02-24 11:43:12 +0100

    fix: clevercloud deploy with htaccess for private documents

--- clevercloud/post_build.sh
+++ clevercloud/post_build.sh
@@ -18,6 +18,10 @@ if test -d ${APP_HOME}/wp-content/themes  ; then
     rsync -a  --delete ${APP_HOME}/wp-content/themes/ ${APP_HOME}${CC_WEBROOT}/wp-content/themes/
 fi
 
+# copy htaccess file to protect private documents
+mkdir -p ${APP_HOME}${CC_WEBROOT}/wp-content/uploads/private
+cp -f ${APP_HOME}/wp-content/uploads/private/.htaccess ${APP_HOME}${CC_WEBROOT}/wp-content/uploads/private/.htaccess
+
 # install restic for FS backup
 RESTIC_TAG=0.17.3
 curl -L https://github.com/restic/restic/releases/download/v${RESTIC_TAG}/restic_${RESTIC_TAG}_linux_amd64.bz2 | bunzip2 > /home/bas/.local/bin/restic
