429ba7f5d77ef405206d9219446cb2b4a977e240
Natacha Heubes <natacha@les-tilleuls.coop>
2026-05-22 15:58:49 +0200

    fix: seo tags in forms legs and foundation

--- private/src/scripts/modules/Form/jetpack-forms.js
+++ private/src/scripts/modules/Form/jetpack-forms.js
@@ -8,11 +8,19 @@ const initJetpackForm = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const codeOrigineValue = urlParams.get('reserved_originecode') ?? urlParams.get('code_origine');
 
-  form.addEventListener('submit', () => {
+  form.addEventListener('submit', (event) => {
     const hiddenInput = form.querySelector('input[name="code_origine"]');
     if (codeOrigineValue) {
       hiddenInput.value = codeOrigineValue;
     }
+    const gtmType = event.currentTarget.getAttribute('data-gtm-type');
+    const gtmName = event.currentTarget.getAttribute('data-gtm-name');
+    window.dataLayer.push({
+      event: 'form_submit',
+      type: gtmType,
+      name: gtmName,
+    });
   });
+
 };
 export default initJetpackForm;
