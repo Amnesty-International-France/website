5889facc87aa9251448009565c27b98b1db7de60
Maxiloud <maxime@les-tilleuls.coop>
2026-05-27 17:18:45 +0200

    Feat - Tunnuel CLH: Calcul dynamique du compteur global de signatures CLH

--- wp-content/themes/humanity-theme/page-changer-leur-histoire.php
+++ wp-content/themes/humanity-theme/page-changer-leur-histoire.php
@@ -15,6 +15,16 @@ if ($is_highlight_time) {
 
     if ($countdown <= 0) {
         $is_highlight_time = false;
+    } else {
+        $total_number_of_signatures_collected = 0;
+        $total_campaign_signatures = 0;
+
+        $list_petition_current_campaign = get_field('list_petition_clh', $post->ID) ?? [];
+
+        foreach ($list_petition_current_campaign as $single_campaign) {
+            $total_campaign_signatures += (int) get_post_meta($single_campaign->ID, 'objectif_signatures', true);
+            $total_number_of_signatures_collected += amnesty_get_petition_signature_count($single_campaign->ID) ?: 0;
+        }
     }
 }
 
@@ -25,6 +35,12 @@ get_header();
     <?php echo render_block(['blockName' => 'core/pattern', 'attrs' => ['slug' => 'amnesty/page-hero']]); ?>
     <div class="wp-block-group container page-container has-gutter">
 		<?php if ($is_highlight_time) : ?>
+			<div>
+				Signature accumulé: <?php echo $total_number_of_signatures_collected; ?>
+			</div>
+			<div>
+				Objectif signature global: <?php echo $total_campaign_signatures; ?>
+			</div>
 			<div id="countdown" data-countdown="<?php echo esc_attr($countdown); ?>">
 				Deadline: <span><?php echo esc_html((string)$countdown); ?></span>
 			</div>
